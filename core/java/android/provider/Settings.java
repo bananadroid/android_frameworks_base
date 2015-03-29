6c3d23af51963f66fbbd56eb60160bd3c8282810
maxwen <max.weninger@gmail.com>
2015-03-29 18:39:55 +0200

    base: allow disable of screenshot shutter sound [1/2]
    @Stallix - Forward port to A13.
    
    Change-Id: I47d52bba21170118af87d35376d81d7569587a2f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5661,6 +5661,11 @@ public final class Settings {
                 "lockscreen_quick_unlock_control";
 
         /**
+         * @hide
+         */
+        public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
