c2d5d8da555e8b392589e726a4cf712574a90b40
Pranav Vashi <neobuddy89@gmail.com>
2017-05-14 08:55:06 +0530

    Custom statusbar logo customizations [1/2]
    * Original placement logic and custom logo patches
      by LorDClockaN and varund7726 respectively.
    * Refresh and resize various icons.
    
    travarilo: Add various icons.
    
    Co-authored-by: Rizky Benggolo <travarilo@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6370,6 +6370,26 @@ public final class Settings {
         public static final String SMART_CHARGING_RESUME_LEVEL = "smart_charging_resume_level";
 
         /**
+         * Statusbar logo
+         * @hide
+         */
+        public static final String STATUS_BAR_LOGO = "status_bar_logo";
+
+        /**
+         * Position of Status bar logo
+         * 0 - Left (default)
+         * 1 - Right
+         * @hide
+         */
+        public static final String STATUS_BAR_LOGO_POSITION = "status_bar_logo_position";
+
+        /**
+         * Statusbar logo custom style
+         * @hide
+         */
+        public static final String STATUS_BAR_LOGO_STYLE = "status_bar_logo_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
