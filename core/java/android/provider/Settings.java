c3351c8fb534639a32b83c6b1c5aa2d8834fd4eb
LorDClockaN <lordclockan@gmail.com>
2017-08-04 15:43:57 +0200

    base: Sensor block per-package switch (1/2)
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Change-Id: I35413fb00d60f3ab11d0039b1cc3a7de799b2626

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6038,6 +6038,12 @@ public final class Settings {
         public static final String LOCKSCREEN_CHARGING_ANIMATION_STYLE = "lockscreen_charging_animation_style";
 
         /**
+         * Sensor block per-package
+         * @hide
+         */
+        public static final String SENSOR_BLOCK = "sensor_block";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
