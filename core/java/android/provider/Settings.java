93a54827bbd74ca6f3e10addbe62403751658fbd
ezio84 <brabus84@gmail.com>
2017-09-01 15:28:27 +0200

    base: Double tap to sleep on lockscreen [1/2]
    Change-Id: Id8638b0716abf8b264ea9dc2b8dceb892e019aa0
    Signed-off-by: palaych <palaych@arrowos.net>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5213,6 +5213,13 @@ public final class Settings {
         public static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
 
         /**
+         * Double tap on lockscreen to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_LOCKSCREEN =
+                "double_tap_sleep_lockscreen";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -5338,6 +5345,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SHOW_BATTERY_PERCENT);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE_VENDOR_HINT);
+            PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_LOCKSCREEN);
         }
 
         /**
