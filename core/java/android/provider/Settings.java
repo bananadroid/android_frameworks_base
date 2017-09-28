f3fa0e45779c86ff106a018322d0abf8da9460da
Decad3nce <decad3nce@gmail.com>
2017-09-28 21:16:26 +0800

    SystemUI: HeadsUp blocklists (1/2)
    @xyyx: reworked

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6589,6 +6589,20 @@ public final class Settings {
         public static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
 
         /**
+         * Applications list where heasdup should't show
+         * @hide
+         */
+        @Readable
+        public static final String HEADS_UP_STOPLIST_VALUES = "heads_up_stoplist_values";
+
+        /**
+         * Which applications to disable heads up notifications for
+         * @hide
+         */
+        @Readable
+        public static final String HEADS_UP_BLACKLIST_VALUES = "heads_up_blacklist_values";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -6725,6 +6739,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(LOCK_HIDE_STATUS_BAR);
             PRIVATE_SETTINGS.add(CUSTOM_UI_TOGGLE);
             PRIVATE_SETTINGS.add(ABOUT_PHONE_HEADER);
+            PRIVATE_SETTINGS.add(HEADS_UP_STOPLIST_VALUES);
+            PRIVATE_SETTINGS.add(HEADS_UP_BLACKLIST_VALUES);
         }
 
         /**
