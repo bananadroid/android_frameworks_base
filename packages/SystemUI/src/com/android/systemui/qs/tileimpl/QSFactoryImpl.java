468861e93b2beac1078a6bd89958e30d6b438095
maxwen <max.weninger@gmail.com>
2018-08-30 22:21:34 +0200

    [1/3] base: SystemUI: keyguard weather view
    Change-Id: Idbedea20d5487a42849c225e3f500aa1f0282c6c
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -294,7 +294,7 @@ public class QSFactoryImpl implements QSFactory {
             case "soundsearch":
                 return mSoundSearchTileProvider.get();
             case "weather":
-                return mWeatherTile.get();
+                return mWeatherTileProvider.get();
         }
 
         // Custom tiles
