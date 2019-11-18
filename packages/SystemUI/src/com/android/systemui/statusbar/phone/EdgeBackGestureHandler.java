c8d8e8acf4f761b000717288007a1d6df8033187
Ali B <abittin@gmail.com>
2019-11-18 23:37:39 +0300

    SystemUI: Allow to hide arrow for back gesture [1/2]
    Thanks to Nicolas Dhouailly <ndhouailly@gmail.com>
    for the original commit for 10 in
    ChangeID Ia1465c7ce47ffa6da719e40140857b94646ac402
    
    Complete rewrite for 11 taking advantage of existing
    	 GestureNavigationSettingsObserver
    
    Change-Id: Ibf0a8083845ca1a171cb8bb8a8569765fc96e99b
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -236,6 +236,8 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
     private boolean mEdgeHapticEnabled;
     private static final int HAPTIC_DURATION = 20;
 
+    private boolean mIsBackGestureArrowEnabled;
+
     private final GestureNavigationSettingsObserver mGestureNavigationSettingsObserver;
 
     private final NavigationEdgeBackPlugin.BackCallback mBackCallback =
@@ -319,6 +321,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
         mEdgeHapticEnabled = mGestureNavigationSettingsObserver.getEdgeHaptic();
         mIsBackGestureAllowed =
                 !mGestureNavigationSettingsObserver.areNavigationButtonForcedVisible();
+        mIsBackGestureArrowEnabled = mGestureNavigationSettingsObserver.getBackArrowGesture();
 
         mYDeadzoneDivider = mGestureNavigationSettingsObserver.getDeadZoneMode();
 
@@ -727,6 +730,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
                     && isWithinTouchRegion((int) ev.getX(), (int) ev.getY());
             if (mAllowGesture) {
                 mEdgeBackPlugin.setIsLeftPanel(mIsOnLeftEdge);
+                mEdgeBackPlugin.setBackArrowVisibility(mIsBackGestureArrowEnabled);
                 mEdgeBackPlugin.onMotionEvent(ev);
             }
             if (mLogGesture) {
