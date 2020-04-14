4fa9f8d1567bfc6416e6d5aa0772046f2351b009
El Dainosor <eldainosor@gmail.com>
2020-04-14 00:09:09 +0000

    Add MNML based clock desgins
    This is two spicy styles brought up by Infinity (AmalD) for his substratum mod for oreo. Recreated as the mod is unfortunately abandoned.
    
    Spicy clocks inspired on his work on MNML Digital Clock Mods, adapted to ClockPlugin, with some small tweaks.
    Source: https://play.google.com/store/apps/details?id=com.amald.digital.clocks
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I8e42b64c12d4e49a0de3a64d90b6a34efb478ef2

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -167,6 +167,8 @@ public final class ClockManager {
         addBuiltinClock(() -> new DividedLinesClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new TuxClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new SfunyClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new MNMLBoxClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new MNMLMinimalClockController(res, layoutInflater, colorExtractor));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
