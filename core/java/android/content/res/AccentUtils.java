99410eb189c4ba90774531610ef1aa6ee0ee5114
SuperDroidBond <superdroidbond@yahoo.com>
2020-05-28 17:48:33 +0530

    RGB Accenter: add more attrs for color changes
    Change-Id: Ia8286fd0727196927adca15ec5c7e0d67bb95f46

--- core/java/android/content/res/AccentUtils.java
+++ core/java/android/content/res/AccentUtils.java
@@ -13,6 +13,14 @@ public class AccentUtils {
 
     private static ArrayList<String> accentResources = new ArrayList<>(
             Arrays.asList("accent_device_default_light",
+                          "material_pixel_blue_dark",
+                          "material_pixel_blue_bright",
+                          "omni_color5",
+                          "omni_color4",
+                          "dialer_theme_color",
+                          "dialer_theme_color_dark",
+                          "dialer_theme_color_20pct",
+                          "colorAccent",
                           "accent_device_default_dark"));
 
     private static final String ACCENT_COLOR_PROP = "persist.sys.theme.accentcolor";
