bebf0b2853346d570935548ab4b0febe77c51795
Christian Oder <myself5@carbonrom.org>
2020-09-04 23:30:39 +0200

    fwb: Rework Typeface handling to only refresh theme when necessary
    Set font at boot completed once and only when changed afterwards, don't
    hardcode true to always refresh theme on Zygote start.
    
    This improve app startup times due to not having to regenerate default Typeface
    and solves app starting issues caused by Typeface changes running OOM.
    
    Also rework the way we send the information to Zygote to also allow clearing
    the USAP pool on change.
    
    Change-Id: I80a92e4fac4a134f48919b7e8a9bcceb528ebc72

--- core/java/com/android/internal/os/ZygoteConnection.java
+++ core/java/com/android/internal/os/ZygoteConnection.java
@@ -196,6 +196,10 @@ class ZygoteConnection {
                             parsedArgs.mHiddenApiAccessStatslogSampleRate);
                 }
 
+                if (parsedArgs.refreshTypeface) {
+                    return handleRefreshTypeface(zygoteServer);
+                }
+
                 if (parsedArgs.mPermittedCapabilities != 0
                         || parsedArgs.mEffectiveCapabilities != 0) {
                     throw new ZygoteSecurityException("Client may not specify capabilities: "
@@ -229,10 +233,6 @@ class ZygoteConnection {
                         throw new IllegalStateException("Unable to set up pipe for invoke-with",
                                 errnoEx);
                     }
-                }
-
-                if (parsedArgs.refreshTheme) {
-                    Typeface.recreateDefaults();
                 }
 
                 /*
@@ -421,6 +421,11 @@ class ZygoteConnection {
     private Runnable handleApiBlacklistExemptions(ZygoteServer zygoteServer, String[] exemptions) {
         return stateChangeWithUsapPoolReset(zygoteServer,
                 () -> ZygoteInit.setApiBlacklistExemptions(exemptions));
+    }
+
+    private Runnable handleRefreshTypeface(ZygoteServer zygoteServer) {
+        return stateChangeWithUsapPoolReset(zygoteServer,
+                () -> Typeface.recreateDefaults());
     }
 
     private Runnable handleUsapPoolStatusChange(ZygoteServer zygoteServer, boolean newStatus) {
