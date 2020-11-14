ff9938a5714c5dd1d05a4f27df5b9dbf99dbc2ce
Joey Huab <joey@evolution-x.org>
2020-11-15 00:31:04 +0900

    Permissions: Grant Personal Safety perms
    Change-Id: Ic9b28a0218227b700453cc768ae901f40e7c2d05

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -1059,6 +1059,10 @@ final class DefaultPermissionGrantPolicy {
                 SENSORS_PERMISSIONS, CONTACTS_PERMISSIONS, ALWAYS_LOCATION_PERMISSIONS,
                 MICROPHONE_PERMISSIONS, PHONE_PERMISSIONS);
 
+        // SafetyHub
+        grantSystemFixedPermissionsToSystemPackage(pm,"com.google.android.apps.safetyhub", userId, SENSORS_PERMISSIONS,
+                CONTACTS_PERMISSIONS, ALWAYS_LOCATION_PERMISSIONS, MICROPHONE_PERMISSIONS, PHONE_PERMISSIONS);
+
         // Settings Services
         grantSystemFixedPermissionsToSystemPackage(pm, "com.google.android.settings.intelligence", userId,
                 PHONE_PERMISSIONS, ALWAYS_LOCATION_PERMISSIONS);
