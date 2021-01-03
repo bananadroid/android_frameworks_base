cc397dc5de857caca85823c7a4e28a4ebb127986
SKULSHADY <SKULSHADY@users.noreply.github.com>
2021-01-03 05:40:26 +0000

    base: Switch styles [1/3]
    Change-Id: I04636632177cba135bd9a56f4e93c6bd7dd4620d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5734,6 +5734,12 @@ public final class Settings {
         public static final String COLOR_BUCKET_OVERLAY = "color_bucket_overlay";
 
         /**
+         * Settings switch button style
+         * @hide
+         */
+        public static final String SWITCH_STYLE = "switch_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
