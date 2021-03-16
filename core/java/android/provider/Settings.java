c970f3a7d358dfdcdb44c7f6bcb959e27c9d8a21
calebcabob <calphonic@gmail.com>
2021-03-16 12:17:32 -0400

    GlobalActions: Allow user to set an optional 4th action [1/2]
    Rather than letting the user jank up their own
    power menu, we make use of the fourth space to
    allow them to pick ONE extra action. They will
    have a list preference with the following:
    - (0) no 4th item
    - (1) screenshot
    - (2) screenrecord
    - (3) flashlight
    
    Adding more options will be simple enough too.
    
    Co-authored-by: maxwen <max.weninger@gmail.com>
    Change-Id: I99a8577a333d9bd509b4034220a418f9dd019109

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4798,6 +4798,17 @@ public final class Settings {
          */
 
         /**
+         * Which action to display in the fourth (user) button in power menu
+         * 0: None
+         * 1: Screenshot
+         * 2: Screenrecord
+         * 3: Flashlight
+         *
+         * @hide
+         */
+        public static final String GLOBAL_ACTIONS_USERS_CHOICE = "global_actions_users_choice";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
