f623638fc8647f466bdddcdf319522eafce248d0
Gegham Zakaryan <zakaryan.2004@outlook.com>
2021-04-01 13:23:57 +0200

    GlobalActionsDialog: Remove broken PhoneStateListener for airplane mode toggle
    The Airplane Mode toggle in GlobalActions is completely broken,
    being stuck in TurningOn transition state when pressing it when
    Airplane Mode is off. This happens because the PhoneStateListener
    doesn't trigger onServiceStateChanged when Airplane Mode turns on.
    This commit removes the redundant PhoneStateListener and makes the
    logic work for Telephony and non-Telephony devices pretty much the
    same way by allowing onAirplaneModeChanged() to handle both cases.
    
    Change-Id: Ifd1e06ff144addf8dd2c8494769b2d646599ae0a
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -74,8 +74,6 @@ import android.provider.Settings;
 import android.service.dreams.IDreamManager;
 import android.sysprop.TelephonyProperties;
 import android.telecom.TelecomManager;
-import android.telephony.PhoneStateListener;
-import android.telephony.ServiceState;
 import android.telephony.TelephonyManager;
 import android.transition.AutoTransition;
 import android.transition.TransitionManager;
@@ -371,7 +369,6 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
         mHasTelephony = connectivityManager.isNetworkSupported(ConnectivityManager.TYPE_MOBILE);
 
         // get notified of phone state changes
-        telephonyManager.listen(mPhoneStateListener, PhoneStateListener.LISTEN_SERVICE_STATE);
         contentResolver.registerContentObserver(
                 Settings.Global.getUriFor(Settings.Global.AIRPLANE_MODE_ON), true,
                 mAirplaneModeObserver);
@@ -2309,19 +2306,6 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
         }
     };
 
-    PhoneStateListener mPhoneStateListener = new PhoneStateListener() {
-        @Override
-        public void onServiceStateChanged(ServiceState serviceState) {
-            if (!mHasTelephony) return;
-            final boolean inAirplaneMode = serviceState.getState() == ServiceState.STATE_POWER_OFF;
-            mAirplaneState = inAirplaneMode ? ToggleState.On : ToggleState.Off;
-            mAirplaneModeOn.updateState(mAirplaneState);
-            mAdapter.notifyDataSetChanged();
-            mOverflowAdapter.notifyDataSetChanged();
-            mPowerAdapter.notifyDataSetChanged();
-        }
-    };
-
     private ContentObserver mAirplaneModeObserver = new ContentObserver(mMainHandler) {
         @Override
         public void onChange(boolean selfChange) {
@@ -2360,9 +2344,6 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
     };
 
     private void onAirplaneModeChanged() {
-        // Let the service state callbacks handle the state.
-        if (mHasTelephony) return;
-
         boolean airplaneModeOn = Settings.Global.getInt(
                 mContentResolver,
                 Settings.Global.AIRPLANE_MODE_ON,
