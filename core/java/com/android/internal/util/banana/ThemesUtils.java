27ad53c31f57cbd93e1a5fd81bd0dc8200b11853
TikkiX2 <radstylemusic@gmail.com>
2021-06-27 13:21:06 +0200

    base: Introduce Android S Style Switches [1/3]
    cause 404 sucks
    
    Change-Id: Ia1b19016edeaee960e0a0b9c0af154394f5e73b2

--- core/java/com/android/internal/util/banana/ThemesUtils.java
+++ core/java/com/android/internal/util/banana/ThemesUtils.java
@@ -41,6 +41,7 @@ public class ThemesUtils {
         "com.android.system.switch.retro", // 5
         "com.android.system.switch.stockish", // 6
         "com.android.system.switch.fluid", // 7
+        "com.android.system.switch.android_s", // 8
     };
 
     public static final String[] NAVBAR_STYLES = {
