TenX-OS/frameworks_base

SystemUI: Add 5 more Custom Statusbar Signal & Wi-Fi icons [1/2]
- Thanks to sis, acherus, iconicicons & cool_modules_69 for these beautiful icons.

Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

core/java/com/android/internal/util/tenx/ThemesUtils.java
@@ -132,6 +132,11 @@ public class ThemesUtils {
         "com.tenx.systemui.signalbar_a",
         "com.tenx.systemui.signalbar_b",
         "com.tenx.systemui.signalbar_c",
+        "com.tenx.systemui.signalbar_d",
+        "com.tenx.systemui.signalbar_e",
+        "com.tenx.systemui.signalbar_f",
+        "com.tenx.systemui.signalbar_g",
+        "com.tenx.systemui.signalbar_h",
     };
 
     // Statusbar Wifi icons
@@ -139,6 +144,11 @@ public class ThemesUtils {
         "com.tenx.systemui.wifibar_a",
         "com.tenx.systemui.wifibar_b",
         "com.tenx.systemui.wifibar_c",
+        "com.tenx.systemui.wifibar_d",
+        "com.tenx.systemui.wifibar_e",
+        "com.tenx.systemui.wifibar_f",
+        "com.tenx.systemui.wifibar_g",
+        "com.tenx.systemui.wifibar_h",
     };
 
     public String[] getTheme(int theme) {

bananadroid/android_frameworks_base

Change-Id: I36e9f8d312d9c971e57d9438eb5a48d0900742d8

core/java/com/android/internal/util/banana/ThemesUtils.java
@@ -59,6 +59,11 @@ public class ThemesUtils {
             "com.custom.systemui.signalbar_a",
             "com.custom.systemui.signalbar_b",
             "com.custom.systemui.signalbar_c",
+            "com.custom.systemui.signalbar_d",
+            "com.custom.systemui.signalbar_e",
+            "com.custom.systemui.signalbar_f",
+            "com.custom.systemui.signalbar_g",
+            "com.custom.systemui.signalbar_h",
     };
 
     // Statusbar Wifi icons
@@ -66,6 +71,11 @@ public class ThemesUtils {
             "com.custom.systemui.wifibar_a",
             "com.custom.systemui.wifibar_b",
             "com.custom.systemui.wifibar_c",
+            "com.custom.systemui.wifibar_d",
+            "com.custom.systemui.wifibar_e",
+            "com.custom.systemui.wifibar_f",
+            "com.custom.systemui.wifibar_g",
+            "com.custom.systemui.wifibar_h",
     };
 
     public static void updateSwitchStyle(IOverlayManager om, int userId, int switchStyle) {