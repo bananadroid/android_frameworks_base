TenX-OS/frameworks_base

base: Custom Logo color options [1/2]
Change-Id: I89a2ac4fb395d259cc8d4176bb30f1be1ef6d077
Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/logo/LogoImageView.java
@@ -44,6 +44,9 @@ public class LogoImageView extends ImageView {
     private int mTenXLogoPosition;
     private int mTenXLogoStyle;
     private int mTintColor = Color.WHITE;
+    private int mLogoColor;
+    private int mLogoColorCustom;
+    private int mAccentColor;
     private final Handler mHandler = new Handler();
     private ContentResolver mContentResolver;
 
@@ -60,6 +63,10 @@ public class LogoImageView extends ImageView {
                     Settings.System.STATUS_BAR_LOGO_POSITION), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_LOGO_STYLE), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_LOGO_COLOR), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_LOGO_COLOR_PICKER), false, this);
         }
 
         @Override
@@ -231,7 +238,15 @@ public class LogoImageView extends ImageView {
 
         clearColorFilter();
 
-        drawable.setTint(mTintColor);
+        if (mLogoColor == 0) {
+            drawable.setTint(mTintColor);
+        } else if (mLogoColor == 1) {
+            mAccentColor =  mContext.getColor(com.android.internal.R.color.gradient_start);
+            setColorFilter(mAccentColor, PorterDuff.Mode.SRC_IN);
+        } else if (mLogoColor == 2) {
+            setColorFilter(mLogoColorCustom, PorterDuff.Mode.SRC_IN);
+        }
+
         setImageDrawable(drawable);
     }
 
@@ -243,6 +258,10 @@ public class LogoImageView extends ImageView {
                 Settings.System.STATUS_BAR_LOGO_POSITION, 0);
         mTenXLogoStyle = Settings.System.getInt(resolver,
                 Settings.System.STATUS_BAR_LOGO_STYLE, 0);
+        mLogoColor = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_LOGO_COLOR, 0);
+        mLogoColorCustom = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_LOGO_COLOR_PICKER, 0xff1a73e8);
         updateTenXLogo();
     }
 }

packages/SystemUI/src/com/android/systemui/statusbar/logo/LogoImageViewRight.java
@@ -44,6 +44,9 @@ public class LogoImageViewRight extends ImageView {
     private int mTenXLogoPosition;
     private int mTenXLogoStyle;
     private int mTintColor = Color.WHITE;
+    private int mLogoColor;
+    private int mLogoColorCustom;
+    private int mAccentColor;
     private final Handler mHandler = new Handler();
     private ContentResolver mContentResolver;
 
@@ -60,6 +63,10 @@ public class LogoImageViewRight extends ImageView {
                     Settings.System.STATUS_BAR_LOGO_POSITION), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_LOGO_STYLE), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_LOGO_COLOR), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_LOGO_COLOR_PICKER), false, this);
         }
 
         @Override
@@ -231,7 +238,14 @@ public class LogoImageViewRight extends ImageView {
 
         clearColorFilter();
 
-        drawable.setTint(mTintColor);
+        if (mLogoColor == 0) {
+            drawable.setTint(mTintColor);
+        } else if (mLogoColor == 1) {
+            mAccentColor =  mContext.getColor(com.android.internal.R.color.gradient_start);
+            setColorFilter(mAccentColor, PorterDuff.Mode.SRC_IN);
+        } else if (mLogoColor == 2) {
+            setColorFilter(mLogoColorCustom, PorterDuff.Mode.SRC_IN);
+        }
         setImageDrawable(drawable);
     }
 
@@ -243,6 +257,10 @@ public class LogoImageViewRight extends ImageView {
                 Settings.System.STATUS_BAR_LOGO_POSITION, 0);
         mTenXLogoStyle = Settings.System.getInt(resolver,
                 Settings.System.STATUS_BAR_LOGO_STYLE, 0);
+        mLogoColor = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_LOGO_COLOR, 0);
+        mLogoColorCustom = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_LOGO_COLOR_PICKER, 0xff1a73e8);
         updateTenXLogo();
     }
 }

bananadroid/android_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/logo/LogoImageView.java
@@ -44,6 +44,9 @@ public class LogoImageView extends ImageView {
     private int mCustomLogoPosition;
     private int mCustomLogoStyle;
     private int mTintColor = Color.WHITE;
+    private int mLogoColor;
+    private int mLogoColorCustom;
+    private int mAccentColor;
     private final Handler mHandler = new Handler();
     private ContentResolver mContentResolver;
 
@@ -60,6 +63,10 @@ public class LogoImageView extends ImageView {
                     Settings.System.STATUS_BAR_LOGO_POSITION), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_LOGO_STYLE), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_LOGO_COLOR), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_LOGO_COLOR_PICKER), false, this);
         }
 
         @Override
@@ -171,7 +178,15 @@ public class LogoImageView extends ImageView {
 
         clearColorFilter();
 
-        drawable.setTint(mTintColor);
+        if (mLogoColor == 0) {
+            drawable.setTint(mTintColor);
+        } else if (mLogoColor == 1) {
+            mAccentColor =  mContext.getColor(com.android.internal.R.color.gradient_start);
+            setColorFilter(mAccentColor, PorterDuff.Mode.SRC_IN);
+        } else if (mLogoColor == 2) {
+            setColorFilter(mLogoColorCustom, PorterDuff.Mode.SRC_IN);
+        }
+
         setImageDrawable(drawable);
     }
 
@@ -183,6 +198,10 @@ public class LogoImageView extends ImageView {
                 Settings.System.STATUS_BAR_LOGO_POSITION, 0);
         mCustomLogoStyle = Settings.System.getInt(resolver,
                 Settings.System.STATUS_BAR_LOGO_STYLE, 0);
+        mLogoColor = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_LOGO_COLOR, 0);
+        mLogoColorCustom = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_LOGO_COLOR_PICKER, 0xfff3d324);
         updateCustomLogo();
     }
 }

packages/SystemUI/src/com/android/systemui/statusbar/logo/LogoImageViewRight.java
@@ -44,6 +44,9 @@ public class LogoImageViewRight extends ImageView {
     private int mCustomLogoPosition;
     private int mCustomLogoStyle;
     private int mTintColor = Color.WHITE;
+    private int mLogoColor;
+    private int mLogoColorCustom;
+    private int mAccentColor;
     private final Handler mHandler = new Handler();
     private ContentResolver mContentResolver;
 
@@ -60,6 +63,10 @@ public class LogoImageViewRight extends ImageView {
                     Settings.System.STATUS_BAR_LOGO_POSITION), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_LOGO_STYLE), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_LOGO_COLOR), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_LOGO_COLOR_PICKER), false, this);
         }
 
         @Override
@@ -171,7 +178,14 @@ public class LogoImageViewRight extends ImageView {
 
         clearColorFilter();
 
-        drawable.setTint(mTintColor);
+        if (mLogoColor == 0) {
+            drawable.setTint(mTintColor);
+        } else if (mLogoColor == 1) {
+            mAccentColor =  mContext.getColor(com.android.internal.R.color.gradient_start);
+            setColorFilter(mAccentColor, PorterDuff.Mode.SRC_IN);
+        } else if (mLogoColor == 2) {
+            setColorFilter(mLogoColorCustom, PorterDuff.Mode.SRC_IN);
+        }
         setImageDrawable(drawable);
     }
 
@@ -183,6 +197,10 @@ public class LogoImageViewRight extends ImageView {
                 Settings.System.STATUS_BAR_LOGO_POSITION, 0);
         mCustomLogoStyle = Settings.System.getInt(resolver,
                 Settings.System.STATUS_BAR_LOGO_STYLE, 0);
+        mLogoColor = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_LOGO_COLOR, 0);
+        mLogoColorCustom = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_LOGO_COLOR_PICKER, 0xfff3d324);
         updateCustomLogo();
     }
 }