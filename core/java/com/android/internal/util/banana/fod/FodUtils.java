athul05/platform_frameworks_base

base: Use FodUtils for FOD config checking

core/java/com/android/internal/util/colt/fod/FodUtils.java
@@ -1,0 +1,25 @@
+/*
+ * Copyright (C) 2019-2020 The Evolution X Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.colt.fod;
+
+import android.content.Context;
+
+public class FodUtils {
+    public static boolean hasFodSupport(Context context) {
+        return context.getResources().getBoolean(com.android.internal.R.bool.config_needCustomFODView);
+    }
+}

packages/SystemUI/src/com/android/systemui/biometrics/FODCircleViewImpl.java
@@ -21,10 +21,10 @@ import android.content.pm.PackageManager;
 import android.util.Slog;
 import android.view.View;
 
+import com.android.internal.util.colt.fod.FodUtils;
+
 import com.android.systemui.SystemUI;
 import com.android.systemui.statusbar.CommandQueue;
-
-import lineageos.app.LineageContextConstants;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -46,7 +46,7 @@ public class FODCircleViewImpl extends SystemUI implements CommandQueue.Callback
     public void start() {
         PackageManager packageManager = mContext.getPackageManager();
         if (!packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT) ||
-                !packageManager.hasSystemFeature(LineageContextConstants.Features.FOD)) {
+                !FodUtils.hasFodSupport(mContext)) {
             return;
         }
         mCommandQueue.addCallback(this);

packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -38,7 +38,6 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.pm.PackageManager;
 import android.content.pm.UserInfo;
 import android.hardware.biometrics.BiometricSourceType;
 import android.media.AudioAttributes;
@@ -75,6 +74,7 @@ import com.android.internal.policy.IKeyguardDrawnCallback;
 import com.android.internal.policy.IKeyguardExitCallback;
 import com.android.internal.policy.IKeyguardStateCallback;
 import com.android.internal.util.LatencyTracker;
+import com.android.internal.util.colt.fod.FodUtils;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.keyguard.KeyguardConstants;
 import com.android.keyguard.KeyguardDisplayManager;
@@ -100,8 +100,6 @@ import com.android.systemui.statusbar.phone.NotificationPanelViewController;
 import com.android.systemui.statusbar.phone.StatusBar;
 import com.android.systemui.util.DeviceConfigProxy;
 import com.android.systemui.util.InjectionInflationController;
-
-import lineageos.app.LineageContextConstants;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -753,8 +751,7 @@ public class KeyguardViewMediator extends SystemUI implements Dumpable {
                 QuickStepContract.isGesturalMode(navigationModeController.addListener(mode -> {
                     mInGestureNavigationMode = QuickStepContract.isGesturalMode(mode);
                 }));
-        PackageManager packageManager = context.getPackageManager();
-        mHasFod = packageManager.hasSystemFeature(LineageContextConstants.Features.FOD);
+        mHasFod = FodUtils.hasFodSupport(context);
     }
 
     public void userActivity() {

services/core/java/com/android/server/biometrics/fingerprint/FingerprintService.java
@@ -64,6 +64,7 @@ import android.util.proto.ProtoOutputStream;
 import com.android.internal.annotations.GuardedBy;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.util.DumpUtils;
+import com.android.internal.util.colt.fod.FodUtils;
 import com.android.server.SystemServerInitThreadPool;
 import com.android.server.biometrics.AuthenticationClient;
 import com.android.server.biometrics.BiometricServiceBase;
@@ -76,8 +77,6 @@ import com.android.server.biometrics.RemovalClient;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import lineageos.app.LineageContextConstants;
 
 import vendor.lineage.biometrics.fingerprint.inscreen.V1_0.IFingerprintInscreen;
 
@@ -811,8 +810,7 @@ public class FingerprintService extends BiometricServiceBase {
         context.registerReceiver(mLockoutReceiver, new IntentFilter(getLockoutResetIntent()),
                 getLockoutBroadcastPermission(), null /* handler */);
 
-        PackageManager packageManager = context.getPackageManager();
-        mHasFod = packageManager.hasSystemFeature(LineageContextConstants.Features.FOD);
+        mHasFod = FodUtils.hasFodSupport(context);
     }
 
     @Override

bananadroid/android_frameworks_base

core/java/com/android/internal/util/banana/fod/FodUtils.java
@@ -1,0 +1,25 @@
+/*
+ * Copyright (C) 2019-2020 The Evolution X Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.banana.fod;
+
+import android.content.Context;
+
+public class FodUtils {
+    public static boolean hasFodSupport(Context context) {
+        return context.getResources().getBoolean(com.android.internal.R.bool.config_needCustomFODView);
+    }
+}

packages/SystemUI/src/com/android/systemui/biometrics/FODCircleViewImpl.java
@@ -23,7 +23,7 @@ import android.os.Looper;
 import android.util.Slog;
 import android.view.View;
 
-import com.android.internal.custom.app.LineageContextConstants;
+import com.android.internal.util.banana.fod.FodUtils;
 
 import com.android.systemui.SystemUI;
 import com.android.systemui.biometrics.FODCircleViewImplCallback;
@@ -60,7 +60,7 @@ public class FODCircleViewImpl extends SystemUI implements CommandQueue.Callback
     public void start() {
         PackageManager packageManager = mContext.getPackageManager();
         if (!packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT) ||
-                !packageManager.hasSystemFeature(LineageContextConstants.Features.FOD)) {
+                !FodUtils.hasFodSupport(mContext)) {
             return;
         }
         mCommandQueue.addCallback(this);

packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -38,7 +38,6 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.pm.PackageManager;
 import android.content.pm.UserInfo;
 import android.hardware.biometrics.BiometricSourceType;
 import android.media.AudioAttributes;
@@ -76,6 +75,7 @@ import com.android.internal.policy.IKeyguardDrawnCallback;
 import com.android.internal.policy.IKeyguardExitCallback;
 import com.android.internal.policy.IKeyguardStateCallback;
 import com.android.internal.util.LatencyTracker;
+import com.android.internal.util.banana.fod.FodUtils;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.keyguard.KeyguardConstants;
 import com.android.keyguard.KeyguardDisplayManager;
@@ -752,8 +752,7 @@ public class KeyguardViewMediator extends SystemUI implements Dumpable {
                 QuickStepContract.isGesturalMode(navigationModeController.addListener(mode -> {
                     mInGestureNavigationMode = QuickStepContract.isGesturalMode(mode);
                 }));
-        PackageManager packageManager = context.getPackageManager();
-        mHasFod = packageManager.hasSystemFeature(LineageContextConstants.Features.FOD);
+        mHasFod = FodUtils.hasFodSupport(context);
     }
 
     public void userActivity() {

services/core/java/com/android/server/biometrics/fingerprint/FingerprintService.java
@@ -63,9 +63,9 @@ import android.util.SparseIntArray;
 import android.util.proto.ProtoOutputStream;
 
 import com.android.internal.annotations.GuardedBy;
-import com.android.internal.custom.app.LineageContextConstants;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.util.DumpUtils;
+import com.android.internal.util.banana.fod.FodUtils;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.server.SystemServerInitThreadPool;
 import com.android.server.biometrics.AuthenticationClient;
@@ -893,8 +893,7 @@ public class FingerprintService extends BiometricServiceBase {
                 getLockoutBroadcastPermission(), null /* handler */);
         mLockPatternUtils = new LockPatternUtils(context);
 
-        PackageManager packageManager = context.getPackageManager();
-        mHasFod = packageManager.hasSystemFeature(LineageContextConstants.Features.FOD);
+        mHasFod = FodUtils.hasFodSupport(context);
     }
 
     @Override