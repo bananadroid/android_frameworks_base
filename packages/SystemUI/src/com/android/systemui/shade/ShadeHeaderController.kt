c351e9a90e883f259c84599f862c7307a82fc39e
Ido Ben-Hur <idoybh2@gmail.com>
2021-11-17 14:42:28 +0200

    QuickStatusBarHeader: Add date click actions
    Adapted to A14 (bring back what was done in/after merges before)
    Clock already had an action programmed in A14 so just follow that code style
    
    Open default clock & calendar accordingly
    
    Change-Id: I2326f942ddb22b572c9d4db1db60d4faa9e13a08

--- packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
+++ packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
@@ -26,11 +26,13 @@ import android.content.Context
 import android.content.res.ColorStateList
 import android.content.res.Configuration
 import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import android.os.Trace
 import android.os.Trace.TRACE_TAG_APP
 import android.os.UserHandle;
 import android.provider.AlarmClock
+import android.provider.CalendarContract
 import android.provider.Settings
 import android.util.Pair
 import android.view.DisplayCutout
@@ -393,6 +395,7 @@ constructor(
                 Intent(Intent.ACTION_POWER_USAGE_SUMMARY), 0
             )
         }
+        date.setOnClickListener { launchDateActivity() }
 
         dumpManager.registerDumpable(this)
         configurationController.addCallback(configurationControllerListener)
@@ -440,6 +443,14 @@ constructor(
         } else {
             activityStarter.postStartActivityDismissingKeyguard(DEFAULT_CLOCK_INTENT, 0 /*delay */)
         }
+    }
+
+    internal fun launchDateActivity() {
+        val builder: Uri.Builder = CalendarContract.CONTENT_URI.buildUpon()
+        builder.appendPath("time")
+        builder.appendPath(System.currentTimeMillis().toString())
+        val todayIntent: Intent = Intent(Intent.ACTION_VIEW, builder.build())
+        activityStarter.postStartActivityDismissingKeyguard(todayIntent, 0 /*delay */)
     }
 
     private fun loadConstraints() {
