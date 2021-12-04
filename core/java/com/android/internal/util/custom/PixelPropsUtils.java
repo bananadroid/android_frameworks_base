043c19462a566bf937c16d9e14bd70face948261
Dobsgw <george.w.dobson@gmail.com>
2021-12-04 11:32:22 +0300

    base: PixelPropsUtils: Whitelist Google Dialer
    With Pixel Props, google dialer doesn't have it's default call recording option.
    Whitelist dialer to get back call recording at the expense of monet support.
    
    Change-Id: I52df29621ec5be1a5e75e15aee0852eddc0405a4

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -90,6 +90,7 @@ public class PixelPropsUtils {
         propsToKeep = new HashMap<>();
         propsToKeep.put("com.google.android.settings.intelligence", new ArrayList<String>(Arrays.asList("FINGERPRINT")));
         propsToKeep.put("com.google.android.GoogleCamera", allProps); 
+        propsToKeep.put("com.google.android.dialer", allProps);
         propsToChangePixel6 = new HashMap<>();
         propsToChangePixel6.put("BRAND", "google");
         propsToChangePixel6.put("MANUFACTURER", "Google");
