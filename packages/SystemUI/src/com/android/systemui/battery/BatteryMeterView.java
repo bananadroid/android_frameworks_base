480b2b46849bf0bfcfd56cd1ad3e616a87760f67
spezi77 <spezi7713@gmx.net>
2022-01-07 21:32:19 +0100

    BatteryMeterView: Fix NPE that occurs when onDensityOrFontScaleChanged() is called.
    Otherwise SysUI will crash.
    
    Tested with "Network traffic indicator".
    
    01-07 22:27:19.805  3347  3347 E AndroidRuntime: FATAL EXCEPTION: main
    01-07 22:27:19.805  3347  3347 E AndroidRuntime: Process: com.android.systemui, PID: 3347
    01-07 22:27:19.805  3347  3347 E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke virtual method 'void android.widget.ImageView.setLayoutParams(android.view.ViewGroup$LayoutParams)' on a null object reference
    01-07 22:27:19.805  3347  3347 E AndroidRuntime: 	at com.android.systemui.BatteryMeterView.scaleBatteryMeterViews(BatteryMeterView.java:497)
    01-07 22:27:19.805  3347  3347 E AndroidRuntime: 	at com.android.systemui.BatteryMeterView.onDensityOrFontScaleChanged(BatteryMeterView.java:437)
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: Ic9378d1177a3ab8e80e1e6d6c9a064b4d626a3e2

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
@@ -348,7 +348,9 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
                 (int) (batteryWidth * iconScaleFactor), (int) (batteryHeight * iconScaleFactor));
         scaledLayoutParams.setMargins(0, 0, 0, marginBottom);
 
-        mBatteryIconView.setLayoutParams(scaledLayoutParams);
+        if (mBatteryIconView != null) {
+            mBatteryIconView.setLayoutParams(scaledLayoutParams);
+        }
     }
 
     @Override
