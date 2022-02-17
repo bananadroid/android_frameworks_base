0749a24df0aaefe12210f8cbdd227351945914c2
Ido Ben-Hur <idoybh2@gmail.com>
2022-02-17 05:03:48 +0200

    base: SystemUI: Fix circle battery QS tinting
    Battery background arc was tinted too close to differ
    Replace textColorSecodary with textColorHint

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -16,6 +16,11 @@ package com.android.systemui.qs;
 
 import static android.app.StatusBarManager.DISABLE2_QUICK_SETTINGS;
 import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+import static com.android.systemui.battery.BatteryMeterView.BATTERY_STYLE_CIRCLE;
+import static com.android.systemui.battery.BatteryMeterView.BATTERY_STYLE_DOTTED_CIRCLE;
+import static com.android.systemui.battery.BatteryMeterView.BATTERY_STYLE_FULL_CIRCLE;
+import static com.android.systemui.battery.BatteryMeterView.BATTERY_STYLE_RLANDSCAPE;
+import static com.android.systemui.battery.BatteryMeterView.BATTERY_STYLE_LANDSCAPE;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -374,6 +379,14 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
             mClockView.setTextColor(textColor);
             if (mTintedIconManager != null) {
                 mTintedIconManager.setTint(textColor);
+            }
+            if ((mBatteryRemainingIcon.getBatteryStyle() == BATTERY_STYLE_CIRCLE)
+                    || (mBatteryRemainingIcon.getBatteryStyle() == BATTERY_STYLE_DOTTED_CIRCLE)
+                    || (mBatteryRemainingIcon.getBatteryStyle() == BATTERY_STYLE_FULL_CIRCLE)
+                    || (mBatteryRemainingIcon.getBatteryStyle() == BATTERY_STYLE_RLANDSCAPE)
+                    || (mBatteryRemainingIcon.getBatteryStyle() == BATTERY_STYLE_LANDSCAPE)) {
+                textColorSecondary = Utils.getColorAttrDefaultColor(mContext,
+                        android.R.attr.textColorHint);
             }
             mBatteryRemainingIcon.updateColors(mTextColorPrimary, textColorSecondary,
                     mTextColorPrimary);
