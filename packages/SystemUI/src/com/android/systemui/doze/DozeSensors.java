956a2e0a0e790592ce918cd5a7f9b43a21629d97
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-02-24 12:21:34 +0530

    DozeSensors: flip logic for disabling prox
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
@@ -117,7 +117,7 @@ public class DozeSensors {
     private boolean mListeningTouchScreenSensors;
     private boolean mListeningProxSensors;
     private boolean mUdfpsEnrolled;
-    private boolean mProximitySupported;
+    private boolean mEnableProx;
 
     @DevicePostureController.DevicePostureInt
     private int mDevicePosture;
@@ -168,7 +168,7 @@ public class DozeSensors {
         mProximitySensor.setTag(TAG);
         mSelectivelyRegisterProxSensors = dozeParameters.getSelectivelyRegisterSensorsUsingProx();
         mListeningProxSensors = !mSelectivelyRegisterProxSensors;
-        mProximitySupported = resources.getBoolean(com.android.systemui.R.bool.doze_proximity_sensor_supported);
+        mEnableProx = resources.getBoolean(com.android.systemui.R.bool.doze_proximity_sensor_supported);
         mScreenOffUdfpsEnabled =
                 config.screenOffUdfpsEnabled(KeyguardUpdateMonitor.getCurrentUser());
         mDevicePostureController = devicePostureController;
@@ -267,8 +267,8 @@ public class DozeSensors {
                         false /* requiresProx */,
                         true /* immediatelyReRegister */),
         };
-        if (mProximitySupported) {
-            setProxListening(false);  // Don't immediately start listening when we register.
+        setProxListening(false);  // Don't immediately start listening when we register.
+        if (mEnableProx) {
             mProximitySensor.register(
                     proximityEvent -> {
                         if (proximityEvent != null) {
@@ -482,7 +482,7 @@ public class DozeSensors {
         for (TriggerSensor s : mTriggerSensors) {
             idpw.println("Sensor: " + s.toString());
         }
-        if (mProximitySupported) // Useless
+        if (mEnableProx) // Useless
             idpw.println("ProxSensor: " + mProximitySensor.toString());
     }
 
@@ -490,7 +490,7 @@ public class DozeSensors {
      * @return true if prox is currently near, false if far or null if unknown.
      */
     public Boolean isProximityCurrentlyNear() {
-        return !mProximitySupported ? null : mProximitySensor.isNear();
+        return mEnableProx ? mProximitySensor.isNear() : null;
     }
 
     @VisibleForTesting
