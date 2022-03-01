7bbec014fb6bc08c6196f1e263b00c15060b7ecf
Pranav Vashi <neobuddy89@gmail.com>
2022-03-01 13:52:37 +0530

    NetworkTraffic: Fix white on white text for Quick Statusbar
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
@@ -505,6 +505,15 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
         }
     }
 
+    public void setTint(int tint) {
+        newTint = tint;
+        // Wait for icon to be visible and tint to be changed
+        if (mVisible && mIconTint != newTint) {
+            mIconTint = newTint;
+            updateTrafficDrawable();
+        }
+    }
+
     protected void updateTrafficDrawable() {
         if (mDrawable != null) {
             mDrawable.setColorFilter(mIconTint, PorterDuff.Mode.MULTIPLY);
