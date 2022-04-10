d3f02b6398edee8a0ceaf2edeee39cf095ff9860
LynnrinChan <wuxiaofei31@gmail.com>
2022-04-10 12:36:08 +0000

    udfps: Disable Night Light when showUdfpsOverlay
    Change-Id: I5a60ea950449e8cb4100c3ab9c053195a4bc719d
    Signed-off-by: LynnrinChan <wuxiaofei31@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -34,6 +34,7 @@ import android.graphics.Point;
 import android.graphics.RectF;
 import android.hardware.biometrics.BiometricOverlayConstants;
 import android.hardware.biometrics.SensorLocationInternal;
+import android.hardware.display.ColorDisplayManager;
 import android.hardware.display.DisplayManager;
 import android.hardware.fingerprint.FingerprintManager;
 import android.hardware.fingerprint.FingerprintSensorPropertiesInternal;
@@ -111,6 +112,7 @@ public class UdfpsController implements DozeReceiver {
     private static final long MIN_TOUCH_LOG_INTERVAL = 50;
 
     private final Context mContext;
+    private final ColorDisplayManager mColorDisplayManager;
     private final Execution mExecution;
     private final FingerprintManager mFingerprintManager;
     @NonNull private final LayoutInflater mInflater;
@@ -168,6 +170,7 @@ public class UdfpsController implements DozeReceiver {
     private boolean mAttemptedToDismissKeyguard;
     private final int mUdfpsVendorCode;
     private Set<Callback> mCallbacks = new HashSet<>();
+    private boolean mNightDisplayEnabled;
 
     @VisibleForTesting
     public static final AudioAttributes VIBRATION_SONIFICATION_ATTRIBUTES =
@@ -634,6 +637,7 @@ public class UdfpsController implements DozeReceiver {
         mConfigurationController = configurationController;
         mSystemClock = systemClock;
         mUnlockedScreenOffAnimationController = unlockedScreenOffAnimationController;
+        mColorDisplayManager = context.getSystemService(ColorDisplayManager.class);
 
         mSensorProps = findFirstUdfps();
         // At least one UDFPS sensor exists
@@ -818,6 +822,10 @@ public class UdfpsController implements DozeReceiver {
     }
 
     private void showUdfpsOverlay(@NonNull ServerRequest request) {
+        mNightDisplayEnabled = mColorDisplayManager.isNightDisplayActivated();
+        if (mNightDisplayEnabled) {
+            mColorDisplayManager.setNightDisplayActivated(false);
+        }
         mExecution.assertIsMainThread();
 
         final int reason = request.mRequestReason;
@@ -923,6 +931,9 @@ public class UdfpsController implements DozeReceiver {
     }
 
     private void hideUdfpsOverlay() {
+        if (mNightDisplayEnabled) {
+            mColorDisplayManager.setNightDisplayActivated(true);
+        }
         mExecution.assertIsMainThread();
 
         if (mView != null) {
