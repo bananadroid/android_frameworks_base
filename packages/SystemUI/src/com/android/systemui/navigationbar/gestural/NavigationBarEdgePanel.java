17be5144f9af5c044b942e24ee86eb5e7cc07c1e
Pranav Vashi <neobuddy89@gmail.com>
2022-04-22 20:23:15 +0530

    Optional haptic feedback on back gesture [1/2]
    This is inspired from
    https://github.com/syberia-project/platform_frameworks_base/commit/2a57b330c1f697ef23460047bd62f6dd20f3368a
    
    However, we do not want to ditch AOSP implementation.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/NavigationBarEdgePanel.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/NavigationBarEdgePanel.java
@@ -231,6 +231,7 @@ public class NavigationBarEdgePanel extends View implements NavigationEdgeBackPl
     private final Runnable mFailsafeRunnable = this::onFailsafe;
 
     private boolean mBackArrowVisibility;
+    private boolean mEdgeHapticEnabled;
 
     private DynamicAnimation.OnAnimationEndListener mSetGoneEndListener
             = new DynamicAnimation.OnAnimationEndListener() {
@@ -436,6 +437,11 @@ public class NavigationBarEdgePanel extends View implements NavigationEdgeBackPl
     @Override
     public void setBackArrowVisibility(boolean backArrowVisibility) {
         mBackArrowVisibility = backArrowVisibility;
+    }
+
+    @Override
+    public void setEdgeHapticEnabled(boolean edgeHapticEnabled) {
+        mEdgeHapticEnabled = edgeHapticEnabled;
     }
 
     /**
@@ -646,8 +652,8 @@ public class NavigationBarEdgePanel extends View implements NavigationEdgeBackPl
         mVelocityTracker.computeCurrentVelocity(1000);
         // Only do the extra translation if we're not already flinging
         boolean isSlow = Math.abs(mVelocityTracker.getXVelocity()) < 500;
-        if (isSlow
-                || SystemClock.uptimeMillis() - mVibrationTime >= GESTURE_DURATION_FOR_CLICK_MS) {
+        if (mEdgeHapticEnabled && (isSlow
+                || SystemClock.uptimeMillis() - mVibrationTime >= GESTURE_DURATION_FOR_CLICK_MS)) {
             mVibratorHelper.vibrate(VibrationEffect.EFFECT_CLICK);
         }
 
@@ -744,8 +750,10 @@ public class NavigationBarEdgePanel extends View implements NavigationEdgeBackPl
         // Apply a haptic on drag slop passed
         if (!mDragSlopPassed && touchTranslation > mSwipeTriggerThreshold) {
             mDragSlopPassed = true;
-            mVibratorHelper.vibrate(VibrationEffect.EFFECT_TICK);
-            mVibrationTime = SystemClock.uptimeMillis();
+            if (mEdgeHapticEnabled) {
+                mVibratorHelper.vibrate(VibrationEffect.EFFECT_TICK);
+                mVibrationTime = SystemClock.uptimeMillis();
+            }
 
             // Let's show the arrow and animate it in!
             mDisappearAmount = 0.0f;
