a10a859330bf6f85a120214c6356ba41dce7ed90
V S Ganga VaraPrasad (VARA) Adabala <quic_vadabala@quicinc.com>
2022-04-26 00:19:43 +0530

    Improve compaction to abort when system changes to awake state
    While the screen is off we sometimes issue compactions as they are not
    disruptive to the user. However, it is possible to wake up and have
    compactions running or scheduled so this may lead to jank. This patch
    cancels any pending or running compactions that were scheduled to happen
    during this non interactive type when the system becomes awake to avoid
    jank.
    
    Test: Manual
    Bug: 214654755
    CRs-Fixed:3121545
    
    Change-Id: Ie9b2cbaa7093d63e77f8666d1e6f050ab5610a1d
    (cherry picked from commit 00fe2b907bf684b0030d1e95b52c84c88d27b48f)
    
    Change-Id: I8dceb1e75562b3e5b33b1aeec3357eaaabfdde4f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/CachedAppOptimizer.java
+++ services/core/java/com/android/server/am/CachedAppOptimizer.java
@@ -28,6 +28,7 @@ import android.net.Uri;
 import android.os.Debug;
 import android.os.Handler;
 import android.os.Message;
+import android.os.PowerManagerInternal;
 import android.os.Process;
 import android.os.SystemClock;
 import android.os.Trace;
@@ -1109,6 +1110,15 @@ public final class CachedAppOptimizer {
                 opt.setPendingFreeze(false);
             }
             mForceFreezePolicy.remove(app);
+        }
+    }
+
+    void onWakefulnessChanged(int wakefulness) {
+        if(wakefulness == PowerManagerInternal.WAKEFULNESS_AWAKE) {
+            // Remove any pending compaction we may have scheduled to happen while screen was off
+            Slog.e(TAG_AM, "Cancel pending or running compactions as system is awake");
+            mPendingCompactionProcesses.clear();
+            cancelCompaction();
         }
     }
 
