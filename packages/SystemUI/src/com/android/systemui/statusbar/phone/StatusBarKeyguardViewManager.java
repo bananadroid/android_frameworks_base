e586aabbe00b40e348afb1f3fb4c385c6e3d0aef
LibXZR <i@xzr.moe>
2022-05-13 21:08:48 +0800

    SystemUI: HACK: Fix random wakeup when just sleeping
    This hack fixes the problem that the device is randomly woke up just
    after screen off to sleep, without changing the original behavior.
    
    Still we need to wait for Google to come up with a real fix.
    
    Change-Id: I5ce99e72bcffe160fca9f4478058d3fcb51eca78
    Signed-off-by: LibXZR <i@xzr.moe>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
@@ -74,6 +74,8 @@ import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.R;
 
 import java.io.PrintWriter;
+import java.lang.StackTraceElement;
+import java.lang.Thread;
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -124,9 +126,18 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
     private final BouncerExpansionCallback mExpansionCallback = new BouncerExpansionCallback() {
         @Override
         public void onFullyShown() {
+            boolean shouldWakeup = true;
+            for (StackTraceElement e : Thread.currentThread().getStackTrace()) {
+                if ("handleShow".equals(e.getMethodName())) {
+                    shouldWakeup = false;
+                    break;
+                }
+            }
             updateStates();
-            mStatusBar.wakeUpIfDozing(SystemClock.uptimeMillis(),
-                    mStatusBar.getBouncerContainer(), "BOUNCER_VISIBLE");
+            if (shouldWakeup) {
+                mStatusBar.wakeUpIfDozing(SystemClock.uptimeMillis(),
+                        mStatusBar.getBouncerContainer(), "BOUNCER_VISIBLE");
+            }
             onKeyguardBouncerFullyShownChanged(true);
             showFaceRecognizingMessage();
         }
