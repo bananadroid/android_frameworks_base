833f610d6a241cd3bf79b32c620a2127a9c0a581
StarkDroid <gamerprince.exp@gmail.com>
2022-06-18 19:53:30 -0500

    Squashed QSPanel style and design changes
    QSPanel: Design changes
    
    * Switch to rounded rectangular tiles
    * Slim designed brightness bar
    
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    
    Add QS Panel Styles [1/3]
    
    Note for other roms:
    This is the jankiest possible implementation that DOES WORK but is more of an extra work.
    Ideally i'd like to have this implemented within in-line ThemeOverLayApplier and ThemeOverlayController but rather
    its being handled through settings with a custom observer at the moment to trigger the overlays.
    
    So why this impl?
    I was just curious to test this out, but once i saw that its implemented and is working, i decided to push it and work on the refinement later
    
    Feel free to do the refinement on your end if you are able to :D
    
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    
    Partial Revert "QSPanel: Design changes"
    
    Prepping this for QS Panel Styles
    
    This reverts commit ef0c7cabca565e8ff4b2f24d5023c8ce9d51f030.
    
    QSTile: More slower & Gradual QS Fade-in/out animation
    
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    
    QS Tile: Set ripple color to accent by default
    
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    
    QS Tile: Make QS Ripple animation more prominent
    
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    
    QS Panel: Add Two-Tone Accented QS Style [1/3]
    
    Inspired by Fluid-UI's QS Panel changes
    
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>
    
    QS Styles: Add QS tiles color pop [1/2]
    
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>
    
    QSPanel Styles: Add key into backup and validator
    
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>
    Signed-off-by: str-br <alexfinhart@gmail.com>
    
    QS Style: Make Outline QS Style truly outline
    
    Change-Id: I2b648d2065a0d9aaa200887027f301625f1ccde7
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>
    mnri: forward port to QPR1, squashed modifications
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
@@ -23,6 +23,7 @@ import android.content.Context
 import android.content.res.ColorStateList
 import android.content.res.Configuration
 import android.content.res.Resources.ID_NULL
+import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.RippleDrawable
 import android.os.Trace
@@ -43,6 +44,7 @@ import android.widget.LinearLayout
 import android.widget.Switch
 import android.widget.TextView
 import androidx.annotation.VisibleForTesting
+import android.annotation.SuppressLint;
 import com.android.settingslib.Utils
 import com.android.systemui.FontSizeUtils
 import com.android.systemui.R
@@ -55,6 +57,10 @@ import com.android.systemui.plugins.qs.QSTileView
 import com.android.systemui.qs.logging.QSLogger
 import com.android.systemui.qs.tileimpl.QSIconViewImpl.QS_ANIM_LENGTH
 import java.util.Objects
+import java.util.Random
+
+
+import android.provider.Settings.System
 
 import com.android.internal.util.banana.BananaUtils.QSLayoutUtils
 
@@ -72,6 +78,8 @@ open class QSTileViewImpl @JvmOverloads constructor(
         private const val SECONDARY_LABEL_NAME = "secondaryLabel"
         private const val CHEVRON_NAME = "chevron"
         const val UNAVAILABLE_ALPHA = 0.3f
+        const val TILE_ALPHA = 0.2f
+        const val INACTIVE_ALPHA = 0.2f
         @VisibleForTesting
         internal const val TILE_STATE_RES_PREFIX = "tile_states_"
     }
@@ -96,6 +104,11 @@ open class QSTileViewImpl @JvmOverloads constructor(
             updateHeight()
         }
 
+    private val qsPanelStyle: Int = System.getIntForUser(
+            context.contentResolver,
+            System.QS_PANEL_STYLE, 0, UserHandle.USER_CURRENT
+        )
+
     private val colorActive = Utils.getColorAttrDefaultColor(context,
             android.R.attr.colorAccent)
     private val colorInactive = Utils.getColorAttrDefaultColor(context, R.attr.offStateColor)
@@ -114,6 +127,24 @@ open class QSTileViewImpl @JvmOverloads constructor(
             Utils.getColorAttrDefaultColor(context, android.R.attr.textColorSecondary)
     private val colorSecondaryLabelUnavailable =
         Utils.getColorAttrDefaultColor(context, com.android.internal.R.attr.textColorTertiary)
+
+    // QS Style 3
+    private val colorActiveAlpha = Utils.applyAlpha(TILE_ALPHA, Utils.getColorAttrDefaultColor(context, android.R.attr.colorAccent))
+    private val colorInactiveAlpha = Utils.applyAlpha(INACTIVE_ALPHA, Utils.getColorAttrDefaultColor(context, R.attr.offStateColor))
+
+    // QS Style 4
+    private var randomColor: Random = Random()
+    
+    @SuppressLint("NewApi")
+    private var randomTint: Int = Color.rgb(
+        (randomColor.nextInt(256) / 2f + 0.5).toFloat(),
+        randomColor.nextInt(256).toFloat(),
+        randomColor.nextInt(256).toFloat()
+    )
+
+    private val colorActiveRandom = Utils.applyAlpha(TILE_ALPHA, randomTint)
+    private val colorLabelActiveRandom = randomTint
+    private val colorSecondaryLabelActiveRandom = randomTint
 
     private lateinit var label: TextView
     protected lateinit var secondaryLabel: TextView
@@ -651,8 +682,13 @@ open class QSTileViewImpl @JvmOverloads constructor(
     private fun getBackgroundColorForState(state: Int, disabledByPolicy: Boolean = false): Int {
         return when {
             state == Tile.STATE_UNAVAILABLE || disabledByPolicy -> colorUnavailable
-            state == Tile.STATE_ACTIVE -> colorActive
-            state == Tile.STATE_INACTIVE -> colorInactive
+            state == Tile.STATE_ACTIVE -> 
+                if(qsPanelStyle == 2) 
+                    colorActiveAlpha 
+                else if(qsPanelStyle == 3) 
+                    colorActiveRandom 
+                else colorActive
+            state == Tile.STATE_INACTIVE -> if(qsPanelStyle >= 1) colorInactiveAlpha else colorInactive
             else -> {
                 Log.e(TAG, "Invalid state $state")
                 0
@@ -663,7 +699,12 @@ open class QSTileViewImpl @JvmOverloads constructor(
     private fun getLabelColorForState(state: Int, disabledByPolicy: Boolean = false): Int {
         return when {
             state == Tile.STATE_UNAVAILABLE || disabledByPolicy -> colorLabelUnavailable
-            state == Tile.STATE_ACTIVE -> colorLabelActive
+            state == Tile.STATE_ACTIVE -> 
+                if(qsPanelStyle == 1 || qsPanelStyle == 2)
+                    colorActive
+                else if(qsPanelStyle == 3) 
+                    colorLabelActiveRandom
+                else colorLabelActive
             state == Tile.STATE_INACTIVE -> colorLabelInactive
             else -> {
                 Log.e(TAG, "Invalid state $state")
@@ -675,7 +716,12 @@ open class QSTileViewImpl @JvmOverloads constructor(
     private fun getSecondaryLabelColorForState(state: Int, disabledByPolicy: Boolean = false): Int {
         return when {
             state == Tile.STATE_UNAVAILABLE || disabledByPolicy -> colorSecondaryLabelUnavailable
-            state == Tile.STATE_ACTIVE -> colorSecondaryLabelActive
+            state == Tile.STATE_ACTIVE -> 
+                if(qsPanelStyle == 1 || qsPanelStyle == 2) 
+                    colorActive
+                else if(qsPanelStyle == 3) 
+                    colorSecondaryLabelActiveRandom
+                else colorSecondaryLabelActive
             state == Tile.STATE_INACTIVE -> colorSecondaryLabelInactive
             else -> {
                 Log.e(TAG, "Invalid state $state")
