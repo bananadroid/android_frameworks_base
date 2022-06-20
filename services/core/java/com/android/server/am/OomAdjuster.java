9c5d510d5f907f9dc4cdc76558f3b07af77daa55
jhonboy121 <alfredmathew05@gmail.com>
2022-06-20 21:00:48 +0530

    services: fix a synchronization issue in CachedAppOptimizer
    Stack trace:
    
    E AndroidRuntime: *** FATAL EXCEPTION IN SYSTEM PROCESS: CachedAppOptimizerThread
    E AndroidRuntime: java.lang.IndexOutOfBoundsException: Index: 0, Size: 0
    E AndroidRuntime: 	at java.util.ArrayList.remove(ArrayList.java:503)
    E AndroidRuntime: 	at com.android.server.am.CachedAppOptimizer$MemCompactionHandler.handleMessage(CachedAppOptimizer.java:1197)
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/OomAdjuster.java
+++ services/core/java/com/android/server/am/OomAdjuster.java
@@ -2621,6 +2621,7 @@ public class OomAdjuster {
         }
     }
 
+    @GuardedBy("mProcLock")
     void onWakefulnessChanged(int wakefulness) {
         mCachedAppOptimizer.onWakefulnessChanged(wakefulness);
     }
