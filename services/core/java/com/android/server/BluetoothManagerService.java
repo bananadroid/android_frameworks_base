3ab7baded328c8f9b763a76960e9740b6e82160f
palaych <palaych@arrowos.net>
2022-06-23 01:54:54 +0900

    base: Revert QTI BT Stack
    Squash revert commits: https://review.arrowos.net/q/project:ArrowOS/android_frameworks_base+branch:arrow-12.0+topic:twelve-qti-bt
    
    Change-Id: I1aad0184063aa9782a5affbcb9564cd63a455b0a

--- services/core/java/com/android/server/BluetoothManagerService.java
+++ services/core/java/com/android/server/BluetoothManagerService.java
@@ -1109,8 +1109,6 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
             }
             if (isBluetoothPersistedStateOnBluetooth() || !isBleAppPresent()) {
                 // This triggers transition to STATE_ON
-                mBluetooth.updateQuietModeStatus(mQuietEnable,
-                        mContext.getAttributionSource());
                 mBluetooth.onLeServiceUp(mContext.getAttributionSource());
                 persistBluetoothSetting(BLUETOOTH_ON_BLUETOOTH);
             }
@@ -1920,8 +1918,6 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
                     mHandler.removeMessages(MESSAGE_RESTART_BLUETOOTH_SERVICE);
                     mEnable = true;
 
-                    mQuietEnable = (msg.arg1 == 1);
-
                     if (isBle == 0) {
                         persistBluetoothSetting(BLUETOOTH_ON_BLUETOOTH);
                     }
@@ -1938,8 +1934,6 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
                                         Slog.i(TAG, "Already at BLE_ON State");
                                     } else {
                                         Slog.w(TAG, "BT Enable in BLE_ON State, going to ON");
-                                        mBluetooth.updateQuietModeStatus(mQuietEnable,
-                                                mContext.getAttributionSource());
                                         mBluetooth.onLeServiceUp(mContext.getAttributionSource());
                                     }
                                     break;
