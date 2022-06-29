9ca2e2faad3fc693dd4884e3b9956e9033de75f4
althafvly <althafvly@gmail.com>
2022-06-29 15:02:37 +0530

    Keyguard: Allow user configurable fingerprint wake-and-unlock
    Change-Id: I471908cfa27a54e0075394eed348cb15386f62ce
    (cherry picked from commit 598811ae53255de1a643c7b5950ec60bcc7136b3)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5636,6 +5636,12 @@ public final class Settings {
         public static final String POWERMENU_EMERGENCY = "powermenu_emergency";
 
         /**
+         * Whether to enable fingerprint wake-and-unlock.
+         * @hide
+         */
+        public static final String FINGERPRINT_WAKE_UNLOCK = "fingerprint_wake_unlock";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
