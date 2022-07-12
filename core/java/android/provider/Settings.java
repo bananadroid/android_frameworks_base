d3191a5c298932248a2bc2d1187d0b75bf2c5039
vjspranav <pranavasri@live.in>
2022-07-13 01:57:15 +0200

    base: Add toggle for custom Settings UI [1/3]
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Change-Id: I5dea89158b0ba45be958c1b3dce94e932f98b87e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6359,6 +6359,15 @@ public final class Settings {
         */
         public static final String QS_FOOTER_WARNINGS = "qs_footer_warnings";
 
+	/**
+          * Enable disable CustomUI
+          *   0 = disabled
+          *   1 = enabled
+          * @author vjspranav
+          * @hide
+          */
+         public static final String CUSTOM_UI_TOGGLE = "custom_ui_toggle";
+
         /**
          * reTicker Status
          * @hide
@@ -6814,6 +6823,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(CAMERA_SLEEP_ON_RELEASE);
             PRIVATE_SETTINGS.add(CAMERA_LAUNCH);
             PRIVATE_SETTINGS.add(LOCK_HIDE_STATUS_BAR);
+            PRIVATE_SETTINGS.add(CUSTOM_UI_TOGGLE);
         }
 
         /**
