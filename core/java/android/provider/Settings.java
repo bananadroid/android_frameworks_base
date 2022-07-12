c8682cb0711b737ca95281ffb0f70f15bb92c29c
vjspranav <pranavasri@live.in>
2022-07-13 01:57:15 +0200

    base: Add toggle for custom Settings UI [1/3]
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Change-Id: I5dea89158b0ba45be958c1b3dce94e932f98b87e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7265,6 +7265,15 @@ public final class Settings {
          */
         public static final String LOCK_GESTURE_STATUS = "lock_gesture_status";
 
+	/**
+          * Enable disable CustomUI
+          *   0 = disabled
+          *   1 = enabled
+          * @author vjspranav
+          * @hide
+          */
+        public static final String CUSTOM_UI_TOGGLE = "custom_ui_toggle";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
@@ -7429,6 +7438,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(KEY_EDGE_LONG_SWIPE_ACTION);
             PRIVATE_SETTINGS.add(BACK_GESTURE_HAPTIC);
             PRIVATE_SETTINGS.add(SENSOR_BLOCK);
+            PRIVATE_SETTINGS.add(CUSTOM_UI_TOGGLE);
         }
 
         /**
