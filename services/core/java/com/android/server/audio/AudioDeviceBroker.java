c7f545d7c969773d937307a6ff41964e52b1fa0e
yaoliang1 <yaoliang1@xiaomi.com>
2022-07-15 16:43:30 +0800

    [Audio][Bugfix]Fix the problem that the initial the volume of headphones does not match the volume of mobile phones in commuication
    bug:239136904
    
    For example, when the phone is connected, the sco device ui on the mobile terminal is 3, and the initial sco volume of the headset is 11. Then when the user adjusts the volume up through the key,the ui of the mobile phone will be 4,but the sound in the bluetooth headset is much smaller than before,and the sound output from the headset is very different from that before.
    
    Signed-off-by: yaoliang1 <yaoliang1@xiaomi.com>
    Change-Id: I3f835e1f44cb25f93d6fac6ccae61fc38192eb98
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/core/java/com/android/server/audio/AudioDeviceBroker.java
+++ services/core/java/com/android/server/audio/AudioDeviceBroker.java
@@ -1888,6 +1888,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
             AudioSystem.setParameters("BT_SCO=off");
         } else {
             AudioSystem.setParameters("BT_SCO=on");
+            mAudioService.postSetBluetoothScoVolume();
         }
         if (preferredCommunicationDevice == null) {
             AudioDeviceAttributes defaultDevice = getDefaultCommunicationDevice();
