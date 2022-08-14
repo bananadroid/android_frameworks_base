f6997fdb1c51cd99dc9ddcaceb3a151754cd5815
elpaablo <donopwhile1@gmail.com>
2022-08-14 03:41:17 +0100

    statusbar: allow user to customize max visible notification icons [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6751,6 +6751,11 @@ public final class Settings {
         };
 
         /**
+         * @hide
+         */
+        public static final String MAX_VISIBLE_NOTIFICATION_ICONS = "max_visible_notification_icons";
+
+        /**
          * These are all public system settings
          *
          * @hide
@@ -6880,6 +6885,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(NAVIGATION_BAR_IME_SPACE);
             PRIVATE_SETTINGS.add(ADAPTIVE_PLAYBACK_ENABLED);
             PRIVATE_SETTINGS.add(ADAPTIVE_PLAYBACK_TIMEOUT);
+            PRIVATE_SETTINGS.add(MAX_VISIBLE_NOTIFICATION_ICONS);
         }
 
         /**
