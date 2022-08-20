6ca468b97ae8f30e2c90e1e312d3012513e7c1dd
LibXZR <i@xzr.moe>
2022-08-20 15:12:23 +0800

    base: Add support for parallel space
    This adds initial support for running multiple instances of the same
    app at the same time. Basically this is done by making use of multiuser.
    
    Further work is needed to make it perfect.
    
    Change-Id: Ib385753df5778a25facd39166abd8b1e29ed7e91
    Signed-off-by: LibXZR <i@xzr.moe>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -86,6 +86,8 @@ import com.android.systemui.util.settings.SystemSettings;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import ink.kaleidoscope.ParallelSpaceManager;
+
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -376,7 +378,9 @@ public class ThemeOverlayController implements CoreStartable, Dumpable, TunerSer
             boolean newWorkProfile = Intent.ACTION_MANAGED_PROFILE_ADDED.equals(intent.getAction());
             boolean isManagedProfile = mUserManager.isManagedProfile(
                     intent.getIntExtra(Intent.EXTRA_USER_HANDLE, 0));
-            if (newWorkProfile) {
+            boolean isParallelSpace =
+                    Intent.ACTION_PARALLEL_SPACE_CHANGED.equals(intent.getAction());
+            if (newWorkProfile || isParallelSpace) {
                 if (!mDeviceProvisionedController.isCurrentUserSetup() && isManagedProfile) {
                     Log.i(TAG, "User setup not finished when " + intent.getAction()
                             + " was received. Deferring... Managed profile? " + isManagedProfile);
@@ -435,6 +439,7 @@ public class ThemeOverlayController implements CoreStartable, Dumpable, TunerSer
         if (DEBUG) Log.d(TAG, "Start");
         final IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_MANAGED_PROFILE_ADDED);
+        filter.addAction(Intent.ACTION_PARALLEL_SPACE_CHANGED);
         filter.addAction(Intent.ACTION_WALLPAPER_CHANGED);
         mBroadcastDispatcher.registerReceiver(mBroadcastReceiver, filter, mMainExecutor,
                 UserHandle.ALL);
@@ -948,7 +953,7 @@ public class ThemeOverlayController implements CoreStartable, Dumpable, TunerSer
                 managedProfiles.add(userInfo.getUserHandle());
             }
         }
-
+        managedProfiles.addAll(ParallelSpaceManager.getInstance().getParallelUserHandles());
         if (DEBUG) {
             Log.d(TAG, "Applying overlays: " + categoryToPackage.keySet().stream()
                     .map(key -> key + " -> " + categoryToPackage.get(key)).collect(
