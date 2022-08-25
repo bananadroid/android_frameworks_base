PixysOS/frameworks_base

base: Add toggle for new Settings UI [1/2]
Signed-off-by: aswin7469 <aswinas@pixysos.com>
Change-Id: I5dea89158b0ba45be958c1b3dce94e932f98b87e

core/java/android/provider/Settings.java
@@ -5560,6 +5560,15 @@ public final class Settings {
          * the setting value. See an example above.
          */
 
+	/**
+          * Enable disable PixysUI
+          *   0 = disabled
+          *   1 = enabled
+          * @author vjspranav
+          * @hide
+          */
+         public static final String PIXYS_UI_TOGGLE = "pixys_ui_toggle";
+
         /**
          * Whether to display sound panel in the power menu
          *
@@ -5820,6 +5829,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(VIBRATE_ON_CALLWAITING);
             PRIVATE_SETTINGS.add(VIBRATE_ON_DISCONNECT);
             PRIVATE_SETTINGS.add(FLASHLIGHT_ON_CALL);
+            PRIVATE_SETTINGS.add(PIXYS_UI_TOGGLE);
             PRIVATE_SETTINGS.add(POCKET_JUDGE);
             PRIVATE_SETTINGS.add(FORCE_FULLSCREEN_CUTOUT_APPS);
             PRIVATE_SETTINGS.add(BUTTON_BACKLIGHT_ONLY_WHEN_PRESSED);

bananadroid/android_frameworks_base

core/java/android/provider/Settings.java
@@ -5960,6 +5960,15 @@ public final class Settings {
          */
         public static final String STATUS_BAR_LOGO_STYLE = "status_bar_logo_style";
 
+	/**
+          * Enable disable CustomUI
+          *   0 = disabled
+          *   1 = enabled
+          * @author vjspranav
+          * @hide
+          */
+         public static final String CUSTOM_UI_TOGGLE = "custom_ui_toggle";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
@@ -6140,6 +6149,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(STATUS_BAR_LOGO);
             PRIVATE_SETTINGS.add(STATUS_BAR_LOGO_POSITION);
             PRIVATE_SETTINGS.add(STATUS_BAR_LOGO_STYLE);
+            PRIVATE_SETTINGS.add(CUSTOM_UI_TOGGLE);
         }
 
         /**
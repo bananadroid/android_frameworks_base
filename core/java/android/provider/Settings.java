ec0bbd087e728ed6ad2c33980d3d0ff27ea32562
vjspranav <pranavasri@live.in>
2022-07-13 01:57:15 +0200

    base: Add toggle for new Settings UI [1/2]
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Change-Id: I5dea89158b0ba45be958c1b3dce94e932f98b87e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5960,6 +5960,15 @@ public final class Settings {
          */
         public static final String STATUS_BAR_LOGO_STYLE = "status_bar_logo_style";
 
+	/**
+          * Enable disable CustomUI
+          *   0 = disabled
+          *   1 = enabled
+          * @author vjspranav
+          * @hide
+          */
+         public static final String CUSTOM_UI_TOGGLE = "custom_ui_toggle";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
@@ -6140,6 +6149,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(STATUS_BAR_LOGO);
             PRIVATE_SETTINGS.add(STATUS_BAR_LOGO_POSITION);
             PRIVATE_SETTINGS.add(STATUS_BAR_LOGO_STYLE);
+            PRIVATE_SETTINGS.add(CUSTOM_UI_TOGGLE);
         }
 
         /**
