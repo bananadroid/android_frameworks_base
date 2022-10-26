da517e1d586cbe9b976d5da2187146a332147f7f
ReallySnow <reallysnow233@gmail.com>
2022-10-26 15:31:02 +0800

    SystemUI: Controllable squiggle animation [1/2]
    * Although the wavy animation is really nice, but some
      users don't like it, so use a switch to control it
      on and off
    * On: https://imgur.com/a/syVpSzh
    * Off: https://imgur.com/a/29s2ZjA
    
    Change-Id: I62a66c848da82d4be8af59df20127c9a96a2df79

--- packages/SystemUI/src/com/android/systemui/media/controls/models/player/SeekBarObserver.kt
+++ packages/SystemUI/src/com/android/systemui/media/controls/models/player/SeekBarObserver.kt
@@ -104,7 +104,7 @@ open class SeekBarObserver(private val holder: MediaViewHolder) :
 
         holder.seekBar.thumb.alpha = if (data.seekAvailable) 255 else 0
         holder.seekBar.isEnabled = data.seekAvailable
-        progressDrawable?.animate = data.playing && !data.scrubbing
+        progressDrawable?.animate = data.playing && !data.scrubbing && data.enableSquiggle
         progressDrawable?.transitionEnabled = !data.seekAvailable
 
         if (holder.seekBar.maxHeight != seekBarEnabledMaxHeight) {
