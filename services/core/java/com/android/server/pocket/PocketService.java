c2d693166824d75c2697a9aa473771a2e510fdc3
Ido Ben-Hur <idoybh2@gmail.com>
2022-12-10 12:54:11 +0200

    base: Pocket Judge: Don't block dialer UI
    What happens is that the dialer looses focus and stops the ringer / vibration
    Listen to calls and never display when we are in one

--- services/core/java/com/android/server/pocket/PocketService.java
+++ services/core/java/com/android/server/pocket/PocketService.java
@@ -39,6 +39,8 @@ import android.pocket.IPocketCallback;
 import android.pocket.PocketConstants;
 import android.pocket.PocketManager;
 import android.provider.Settings.System;
+import android.telephony.TelephonyCallback;
+import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.util.Slog;
@@ -127,13 +129,15 @@ public class PocketService extends SystemService implements IBinder.DeathRecipie
      */
     private static final float POCKET_LIGHT_MAX_THRESHOLD = 3.0f;
 
-    private final ArrayList<IPocketCallback> mCallbacks= new ArrayList<>();
+    private final ArrayList<IPocketCallback> mCallbacks = new ArrayList<>();
+    private final TelephonyManager mTelephonyManager;
 
     private Context mContext;
     private boolean mEnabled;
     private boolean mSystemReady;
     private boolean mSystemBooted;
     private boolean mInteractive;
+    private boolean mIsOnCall;
     private boolean mPending;
     private PocketHandler mHandler;
     private PocketObserver mObserver;
@@ -189,11 +193,21 @@ public class PocketService extends SystemService implements IBinder.DeathRecipie
         }
         mVendorSensor = getSensor(mSensorManager, mVendorPocketSensor);
         mSupportedByDevice = mContext.getResources().getBoolean(
-                                 com.android.internal.R.bool.config_pocketModeSupported);
+                com.android.internal.R.bool.config_pocketModeSupported);
         mObserver = new PocketObserver(mHandler);
         if (mSupportedByDevice){
             mObserver.onChange(true);
             mObserver.register();
+        }
+
+        mTelephonyManager = (TelephonyManager)
+                context.getSystemService(Context.TELEPHONY_SERVICE);
+        if (mSupportedByDevice){
+            mTelephonyManager.registerTelephonyCallback(
+                TelephonyManager.INCLUDE_LOCATION_DATA_NONE,
+                context.getMainExecutor(),
+                mOnCallStateListener
+            );
         }
     }
 
@@ -227,6 +241,18 @@ public class PocketService extends SystemService implements IBinder.DeathRecipie
             }
         }
 
+    }
+
+    final TelephonyCallback mOnCallStateListener = new OnCallStateListener();
+    private class OnCallStateListener extends TelephonyCallback
+            implements TelephonyCallback.CallStateListener {
+        @Override
+        public void onCallStateChanged(int state) {
+            final boolean call = state != TelephonyManager.CALL_STATE_IDLE;
+            final boolean changed = call != mIsOnCall;
+            mIsOnCall = call;
+            if (changed) update();
+        }
     }
 
     private class PocketHandler extends Handler {
@@ -333,6 +359,7 @@ public class PocketService extends SystemService implements IBinder.DeathRecipie
         }
         unregisterSensorListeners();
         mObserver.unregister();
+        mTelephonyManager.unregisterTelephonyCallback(mOnCallStateListener);
     }
 
     private final class PocketServiceWrapper extends IPocketService.Stub {
@@ -493,8 +520,8 @@ public class PocketService extends SystemService implements IBinder.DeathRecipie
         if (!mSupportedByDevice){
             return;
         }
-        if (!mEnabled || mInteractive) {
-            if (mEnabled && isDeviceInPocket()) {
+        if (!mEnabled || mInteractive || mIsOnCall) {
+            if (mEnabled && isDeviceInPocket() && !mIsOnCall) {
                 // if device is judged to be in pocket while switching
                 // to interactive state, we need to keep monitoring.
                 return;
@@ -503,6 +530,7 @@ public class PocketService extends SystemService implements IBinder.DeathRecipie
         } else {
             mHandler.removeMessages(PocketHandler.MSG_UNREGISTER_TIMEOUT);
             registerSensorListeners();
+            handleDispatchCallbacks();
         }
     }
 
