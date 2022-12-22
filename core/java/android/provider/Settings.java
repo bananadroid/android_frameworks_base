b395f10c3a64cff8c2f59f4f956fea344e040e3b
Rizky Benggolo <travarilo@gmail.com>
2022-12-22 18:03:58 +0700

    base: Add toggle for about phone header style [1/3]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6368,6 +6368,12 @@ public final class Settings {
           */
          public static final String CUSTOM_UI_TOGGLE = "custom_ui_toggle";
 
+	/**
+          * About phone header style on Settings
+          * @hide
+          */
+         public static final String ABOUT_PHONE_HEADER = "about_phone_header";
+
         /**
          * reTicker Status
          * @hide
@@ -6824,6 +6830,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(CAMERA_LAUNCH);
             PRIVATE_SETTINGS.add(LOCK_HIDE_STATUS_BAR);
             PRIVATE_SETTINGS.add(CUSTOM_UI_TOGGLE);
+            PRIVATE_SETTINGS.add(ABOUT_PHONE_HEADER);
         }
 
         /**
