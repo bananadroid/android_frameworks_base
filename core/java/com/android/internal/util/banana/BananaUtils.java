f474b9bce2b6bfffb27379873879da98bea0fd26
spkal01 <kalligeross@gmail.com>
2022-12-29 21:06:42 +0800

    Utils: Import Settings restart prompt api
    mnri: resolved memory leaks
    
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/com/android/internal/util/banana/BananaUtils.java
+++ core/java/com/android/internal/util/banana/BananaUtils.java
@@ -54,6 +54,7 @@ import android.view.KeyEvent;
 import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.R;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -352,6 +353,50 @@ public class BananaUtils {
                 for (ActivityManager.RunningAppProcessInfo app: am.getRunningAppProcesses()) {
                     if ("com.android.systemui".equals(app.processName)) {
                         ams.killApplicationProcess(app.processName, app.uid);
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+   public static void showSettingsRestartDialog(Context context) {
+        new AlertDialog.Builder(context)
+                .setTitle(R.string.settings_restart_title)
+                .setMessage(R.string.settings_restart_message)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        restartSettings(context);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .show();
+    }
+
+    public static void restartSettings(Context context) {
+        new restartSettingsTask(context).execute();
+    }
+
+    private static class restartSettingsTask extends AsyncTask<Void, Void, Void> {
+        private WeakReference<Context> mContext;
+
+        public restartSettingsTask(Context context) {
+            super();
+            mContext = new WeakReference<>(context);
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                ActivityManager am =
+                        (ActivityManager) mContext.get().getSystemService(Context.ACTIVITY_SERVICE);
+                IActivityManager ams = ActivityManager.getService();
+                for (ActivityManager.RunningAppProcessInfo app: am.getRunningAppProcesses()) {
+                    if ("com.android.settings".equals(app.processName)) {
+                    	ams.killApplicationProcess(app.processName, app.uid);
                         break;
                     }
                 }
