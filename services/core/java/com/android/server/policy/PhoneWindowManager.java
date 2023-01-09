f31fe7b0b67778c479dc3c234e55a85347a2e055
Ido Ben-Hur <idoybh2@gmail.com>
2023-01-09 03:38:02 +0200

    base: Pocket Judge: Don't go to sleep while on call
    This causes gdialer to stop vibrations. Let's not.
    
    Also:
    * Properly resume listening on fingerprints
    * Use TYPE_SECURE_SYSTEM_OVERLAY for the pocket lock view to actually overlay above everything
      (see com.android.server.policy.WindowManagerPolicy for a full list of priorities)
    
    This commit also reverts previous temp solutions to this issue:
    
    Revert "base: Allow enabling pocket judge on call via an overlay"
    
    This reverts commit 52a12f033992a454b9af6f7ea1c1ea5e2a9f9b07.
    
    Revert "base: Pocket Judge: Don't block dialer UI"
    
    This reverts commit e9dff2d370237ff98bdb7ee07f58239869098807.

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -704,7 +704,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private PocketLock mPocketLock;
     private boolean mPocketLockShowing;
     private boolean mIsDeviceInPocket;
-    private boolean mIsPocketDisabledOnCall;
     private final IPocketCallback mPocketCallback = new IPocketCallback.Stub() {
 
         @Override
@@ -2249,8 +2248,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         Resources res = mContext.getResources();
         mWakeOnDpadKeyPress =
                 res.getBoolean(com.android.internal.R.bool.config_wakeOnDpadKeyPress);
-        mIsPocketDisabledOnCall =
-                res.getBoolean(com.android.internal.R.bool.config_pocketJudgeDisableOnCall);
 
         // Init display burn-in protection
         boolean burnInProtectionEnabled = context.getResources().getBoolean(
@@ -5777,13 +5774,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (!mSystemReady || !mSystemBooted || !mKeyguardDrawnOnce
                 || mPocketLock == null || mPocketLockShowing) {
             return;
-        }
-
-        if (mIsPocketDisabledOnCall) {
-            TelecomManager telecomManager = getTelecommService();
-            if (telecomManager != null && telecomManager.isInCall()) {
-                return;
-            }
         }
 
         if (mPowerManager.isInteractive() && !isKeyguardShowingAndNotOccluded()){
