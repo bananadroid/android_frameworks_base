8a43946933b2d7cffd5f67649593a7661651b441
Joey Huab <joey@evolution-x.org>
2023-02-06 18:24:47 +0000

    PixelPropsUtils: Update fingerprints to February 2023 release
    Change-Id: I1d79ac67e6351d11064bf59424a58d14beb09847

--- core/java/com/android/internal/util/banana/PixelPropsUtils.java
+++ core/java/com/android/internal/util/banana/PixelPropsUtils.java
@@ -192,7 +192,7 @@ public class PixelPropsUtils {
         propsToChangePixel7Pro.put("DEVICE", "cheetah");
         propsToChangePixel7Pro.put("PRODUCT", "cheetah");
         propsToChangePixel7Pro.put("MODEL", "Pixel 7 Pro");
-        propsToChangePixel7Pro.put("FINGERPRINT", "google/cheetah/cheetah:13/TQ1A.230105.001.A2/932567:user/release-keys");
+        propsToChangePixel7Pro.put("FINGERPRINT", "google/cheetah/cheetah:13/TQ1A.230205.002/9471150:user/release-keys");
         propsToChangePixel7 = new HashMap<>();
         propsToChangePixel7.put("BRAND", "google");
         propsToChangePixel7.put("MANUFACTURER", "Google");
@@ -206,7 +206,7 @@ public class PixelPropsUtils {
         propsToChangePixel5.put("DEVICE", "redfin");
         propsToChangePixel5.put("PRODUCT", "redfin");
         propsToChangePixel5.put("MODEL", "Pixel 5");
-        propsToChangePixel5.put("FINGERPRINT", "google/redfin/redfin:13/TQ1A.230105.001/9292298:user/release-keys");
+        propsToChangePixel5.put("FINGERPRINT", "google/redfin/redfin:13/TQ1A.230205.002/9471150:user/release-keys");
         propsToChangePixelXL = new HashMap<>();
         propsToChangePixelXL.put("BRAND", "google");
         propsToChangePixelXL.put("MANUFACTURER", "Google");
