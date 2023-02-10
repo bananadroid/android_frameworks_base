28fcb36e636f243bfe0fc44798b32e0c2ea4926b
Jakub Tyszkowski <tyszkowski@google.com>
2023-02-10 14:34:03 +0000

    AudioService: Update the volume after voip call ends
    Bug: 247659576
    Tag: #feature
    Test: manually testing with VoIP call while streaming media
    Change-Id: I9b6e16234a988ec48474789f224c5454cf589389

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -4001,6 +4001,7 @@ public class AudioService extends IAudioService.Stub
         }
         if (mVoicePlaybackActive.getAndSet(voiceActive) != voiceActive) {
             updateHearingAidVolumeOnVoiceActivityUpdate();
+            updateLeAudioVolumeOnVoiceActivityUpdate();
         }
         if (mMediaPlaybackActive.getAndSet(mediaActive) != mediaActive && mediaActive) {
             scheduleMusicActiveCheck();
@@ -4130,6 +4131,19 @@ public class AudioService extends IAudioService.Stub
                 mVoicePlaybackActive.get(), streamType, index));
         mDeviceBroker.postSetHearingAidVolumeIndex(index * 10, streamType);
 
+    }
+
+    private void updateLeAudioVolumeOnVoiceActivityUpdate() {
+        final int streamType = getBluetoothContextualVolumeStream();
+        final int index = getStreamVolume(streamType);
+        final int maxIndex = getMaxVssVolumeForStream(streamType);
+
+        if (DEBUG_VOL) {
+            Log.d(TAG, "updateLeAudioVolumeOnVoiceActivityUpdate device="
+                    + getDeviceForStream(streamType) + ", mode=" + mMode.get() + ", index=" + index
+                    + " maxIndex=" + maxIndex + " streamType=" + streamType);
+        }
+        mDeviceBroker.postSetLeAudioVolumeIndex(index, maxIndex, streamType);
     }
 
     /**
