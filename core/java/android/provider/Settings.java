36e5d1445ea122c19be23ff767b3db5a966c84e0
Rizky Benggolo <travarilo@gmail.com>
2023-02-12 23:36:20 +0700

    base: Remove qs header clock size preference
    Change-Id: I931748b4c2a16cf57a42845eae6299ebf60768d2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5905,12 +5905,6 @@ public final class Settings {
         public static final String STATUS_BAR_CLOCK_SIZE = "status_bar_clock_size";
 
         /**
-         * Clock font size QS
-         * @hide
-         */
-        public static final String QS_HEADER_CLOCK_SIZE = "qs_header_clock_size";
-
-        /**
          * Statusbar clock background
          * 0 - hide accented chip  (default)
          * 1 - show accented chip
--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -106,8 +106,6 @@ public class Clock extends TextView implements
             "system:" + Settings.System.STATUS_BAR_CLOCK_AUTO_HIDE_SDURATION;
     public static final String STATUS_BAR_CLOCK_SIZE =
             "system:" + Settings.System.STATUS_BAR_CLOCK_SIZE;
-    public static final String QS_HEADER_CLOCK_SIZE =
-            "system:" + Settings.System.QS_HEADER_CLOCK_SIZE;
 
     private int mClockSize;
     private int mClockSizeQsHeader;
@@ -273,8 +271,7 @@ public class Clock extends TextView implements
                     STATUS_BAR_CLOCK_AUTO_HIDE,
                     STATUS_BAR_CLOCK_AUTO_HIDE_HDURATION,
                     STATUS_BAR_CLOCK_AUTO_HIDE_SDURATION,
-                    STATUS_BAR_CLOCK_SIZE,
-                    QS_HEADER_CLOCK_SIZE);
+                    STATUS_BAR_CLOCK_SIZE);
             mCommandQueue.addCallback(this);
             if (mShowDark) {
                 Dependency.get(DarkIconDispatcher.class).addDarkReceiver(this);
@@ -510,11 +507,6 @@ public class Clock extends TextView implements
                 break;
             case STATUS_BAR_CLOCK_SIZE:
                 mClockSize =
-                        TunerService.parseInteger(newValue, 14);
-                updateClockSize();
-                break;
-            case QS_HEADER_CLOCK_SIZE:
-                mClockSizeQsHeader =
                         TunerService.parseInteger(newValue, 14);
                 updateClockSize();
                 break;
@@ -791,10 +783,6 @@ public class Clock extends TextView implements
     };
 
     public void updateClockSize() {
-        if (mQsHeader) {
-            setTextSize(mClockSizeQsHeader);
-        } else {
-            setTextSize(mClockSize);
-        }
+        setTextSize(mClockSize);
     }
 }
