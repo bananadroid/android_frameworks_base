e6de5860df3de9ef9317fb8637895eb98e0ad569
minaripenguin <minaripenguin@users.noreply.github.com>
2023-02-14 08:06:17 +0800

    ActivityThread: Fix a race condition in content provider
    * attempt to fix music players like poweramp not playing unless launched or running on foreground
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -436,6 +436,8 @@ public final class ActivityThread extends ClientTransactionHandler
     Configuration mPendingConfiguration = null;
     // An executor that performs multi-step transactions.
     private final TransactionExecutor mTransactionExecutor = new TransactionExecutor(this);
+    
+    private boolean mProviderAcquired = false;
 
     @UnsupportedAppUsage(maxTargetSdk = Build.VERSION_CODES.P, trackingBug = 115609023)
     private final ResourcesManager mResourcesManager;
@@ -1893,6 +1895,7 @@ public final class ActivityThread extends ClientTransactionHandler
                 synchronized (key.mLock) {
                     key.mHolder = holder;
                     key.mLock.notifyAll();
+                    mProviderAcquired = true;
                 }
             }
         }
@@ -7080,18 +7083,19 @@ public final class ActivityThread extends ClientTransactionHandler
                 // local, we'll need to wait for the publishing of the provider.
                 if (holder != null && holder.provider == null && !holder.mLocal) {
                     synchronized (key.mLock) {
-                        if (key.mHolder != null) {
-                            if (DEBUG_PROVIDER) {
-                                Slog.i(TAG, "already received provider: " + auth);
-                            }
+                        if (mProviderAcquired && key.mHolder != null) {
+                                mProviderAcquired = true;
                         } else {
-                            key.mLock.wait(ContentResolver.CONTENT_PROVIDER_READY_TIMEOUT_MILLIS);
+                            if (!mProviderAcquired) {
+                                key.mLock.wait(ContentResolver.CONTENT_PROVIDER_READY_TIMEOUT_MILLIS);
+                            }
                         }
                         holder = key.mHolder;
                     }
                     if (holder != null && holder.provider == null) {
                         // probably timed out
                         holder = null;
+                        mProviderAcquired = false;
                     }
                 }
             }
@@ -7099,13 +7103,15 @@ public final class ActivityThread extends ClientTransactionHandler
             throw ex.rethrowFromSystemServer();
         } catch (InterruptedException e) {
             holder = null;
+            mProviderAcquired = false;
         } finally {
             // Clear the holder from the key since the key itself is never cleared.
             synchronized (key.mLock) {
                 key.mHolder = null;
+                mProviderAcquired = false;
             }
         }
-        if (holder == null) {
+        if (holder == null && !mProviderAcquired) {
             return null;
         }
 
