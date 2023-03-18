50d801c92a8c001567381c159d18ffc033e1ef46
NurKeinNeid <mralexman3000@gmail.com>
2023-03-18 08:57:19 +0100

    Revert "Separate notification/ring volume sliders in UI"
    This reverts commit 60cd96f8dac2fbfb4e4ca17aae3a13272b4c2182.

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -4907,13 +4907,6 @@ public class NotificationManagerService extends SystemService {
         }
 
         @Override
-        public int getHintsFromListenerNoToken() {
-            synchronized (mNotificationLock) {
-                return mListenerHints;
-            }
-        }
-
-        @Override
         public void requestInterruptionFilterFromListener(INotificationListener token,
                 int interruptionFilter) throws RemoteException {
             final long identity = Binder.clearCallingIdentity();
