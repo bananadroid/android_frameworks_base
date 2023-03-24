ce476534042fca4711aea109d84005a770e6c85f
minaripenguin <minaripenguin@users.noreply.github.com>
2023-03-24 12:20:29 +0800

    ViewConfiguration: Fix multi-thread safety issue
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/view/ViewConfiguration.java
+++ core/java/android/view/ViewConfiguration.java
@@ -37,6 +37,8 @@ import android.util.DisplayMetrics;
 import android.util.SparseArray;
 import android.util.TypedValue;
 
+import com.android.internal.annotations.GuardedBy;
+
 /**
  * Contains methods to standard constants used in the UI for timeouts, sizes, and distances.
  */
@@ -362,12 +364,14 @@ public class ViewConfiguration {
     private final int mSmartSelectionInitializedTimeout;
     private final int mSmartSelectionInitializingTimeout;
     private final boolean mPreferKeepClearForFocusEnabled;
+    private static final Object sLock = new Object();
 
     @UnsupportedAppUsage(maxTargetSdk = Build.VERSION_CODES.P, trackingBug = 123768915)
     private boolean sHasPermanentMenuKey;
     @UnsupportedAppUsage
     private boolean sHasPermanentMenuKeySet;
 
+    @GuardedBy("sLock")
     @UnsupportedAppUsage
     static final SparseArray<ViewConfiguration> sConfigurations =
             new SparseArray<ViewConfiguration>(2);
@@ -548,11 +552,15 @@ public class ViewConfiguration {
 
         final DisplayMetrics metrics = context.getResources().getDisplayMetrics();
         final int density = (int) (100.0f * metrics.density);
+	
+	ViewConfiguration configuration;
 
-        ViewConfiguration configuration = sConfigurations.get(density);
-        if (configuration == null) {
-            configuration = new ViewConfiguration(context);
-            sConfigurations.put(density, configuration);
+	synchronized (sLock) {
+           configuration = sConfigurations.get(density);
+           if (configuration == null) {
+              configuration = new ViewConfiguration(context);
+              sConfigurations.put(density, configuration);
+          }
         }
 
         return configuration;
