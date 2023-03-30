074a8f733de5965a121ae6a85a57f8bb2638c362
minaripenguin <minaripenguin@users.noreply.github.com>
2023-03-30 22:33:05 +0800

    SystemUI: QS Header Image [1/2]
    * reverse engineered from ancient os with [1] as reference for broken codes from jadx
    * Optimized qs banner resources for systemui file size
    * all credits goes to ancientOS
    
    [1]: https://github.com/Hikari-no-Tenshi/crdroid_android_frameworks_base/commit/354d981d3f79783e284c8aced755e90496324982
    
    mnri: rewrite and refactored for risingOS
    
    Co-authored-by: Idc <59904122+IDontCare-05@users.noreply.github.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6879,6 +6879,12 @@ public final class Settings {
         public static final String MEDIA_ARTWORK_FADE_PERCENT = "media_artwork_fade_percent";
 
         /**
+         * QS Header
+         * @hide
+         */
+        public static final String QS_HEADER_IMAGE = "qs_header_image";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
