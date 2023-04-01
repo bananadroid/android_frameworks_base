1551d117e3b0ce4ad48b42a7df2ed108405dafa6
Max Weninger <max.weninger@gmail.com>
2023-04-01 16:14:24 +0800

    SystemUI: Lockscreen clock color settings [1/2]
    * original commit: omnirom/android_frameworks_base@3a796af
    
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12278,6 +12278,16 @@ public final class Settings {
         public static final String NAVBAR_INVERSE_LAYOUT = "navbar_inverse_layout";
 
         /**
+         * @hide
+         */
+        public static final String KG_CUSTOM_CLOCK_COLOR = "kg_custom_clock_color";
+
+        /**
+         * @hide
+         */
+        public static final String KG_CUSTOM_CLOCK_COLOR_ENABLED = "kg_custom_clock_color_enabled";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
