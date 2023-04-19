0afe4e34e0f4b9b676d875c3c417e6f9c20f4681
Rizky Benggolo <travarilo@gmail.com>
2023-04-19 18:55:32 +0700

    Revert "frameworks: Add unlinked ringtone and notification volumes"
    This reverts commit 38825038c7d3d2e9ebe71af58e548dd302b7b2a5.
    
    Revert "AudioService: Fix issue with linked notification volume"
    
    This reverts commit 9892f6d3ffd790d0bed59b6956e12eaffd4df324.

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -843,9 +843,6 @@ public class AudioService extends IAudioService.Stub
         0.9f,   // Pre-scale for index 3
     };
 
-    private boolean mLinkNotificationWithVolume;
-    private final boolean mVoiceCapable;
-
     private NotificationManager mNm;
     private AudioManagerInternal.RingerModeDelegate mRingerModeDelegate;
     private VolumePolicy mVolumePolicy = VolumePolicy.DEFAULT;
@@ -1205,9 +1202,6 @@ public class AudioService extends IAudioService.Stub
             }
         }
 
-        mVoiceCapable = context.getResources().getBoolean(
-                com.android.internal.R.bool.config_voice_capable);
-
         if (looper == null) {
             createAudioSystemThread();
         } else {
@@ -1238,10 +1232,6 @@ public class AudioService extends IAudioService.Stub
                 com.android.internal.R.integer.config_safe_media_volume_index) * 10;
 
         mAlarmManager = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
-
-        // read this in before readPersistedSettings() because updateStreamVolumeAlias needs it
-        mLinkNotificationWithVolume = Settings.Secure.getInt(mContext.getContentResolver(),
-                Settings.Secure.VOLUME_LINK_NOTIFICATION, 1) == 1;
 
         mUseFixedVolume = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_useFixedVolume);
@@ -2303,15 +2293,6 @@ public class AudioService extends IAudioService.Stub
         mStreamVolumeAlias[AudioSystem.STREAM_ACCESSIBILITY] = a11yStreamAlias;
         mStreamVolumeAlias[AudioSystem.STREAM_ASSISTANT] = assistantStreamAlias;
 
-        if (mVoiceCapable) {
-            if (mLinkNotificationWithVolume) {
-                mStreamVolumeAlias[AudioSystem.STREAM_NOTIFICATION] = AudioSystem.STREAM_RING;
-            } else {
-                mStreamVolumeAlias[AudioSystem.STREAM_NOTIFICATION] =
-                        AudioSystem.STREAM_NOTIFICATION;
-            }
-        }
-
         if (updateVolumes && mStreamStates != null) {
             updateDefaultVolumes();
 
@@ -2766,9 +2747,6 @@ public class AudioService extends IAudioService.Stub
             resetActiveAssistantUidsLocked();
             AudioSystem.setRttEnabled(mRttEnabled);
         }
-
-        mLinkNotificationWithVolume = Settings.Secure.getInt(cr,
-                Settings.Secure.VOLUME_LINK_NOTIFICATION, 1) == 1;
 
         mMuteAffectedStreams = mSettings.getSystemIntForUser(cr,
                 System.MUTE_STREAMS_AFFECTED, AudioSystem.DEFAULT_MUTE_STREAMS_AFFECTED,
@@ -8885,8 +8863,6 @@ public class AudioService extends IAudioService.Stub
                     Settings.System.MASTER_MONO), false, this);
             mContentResolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.MASTER_BALANCE), false, this);
-            mContentResolver.registerContentObserver(Settings.Secure.getUriFor(
-                    Settings.Secure.VOLUME_LINK_NOTIFICATION), false, this);
 
             mEncodedSurroundMode = mSettings.getGlobalInt(
                     mContentResolver, Settings.Global.ENCODED_SURROUND_OUTPUT,
@@ -8923,13 +8899,6 @@ public class AudioService extends IAudioService.Stub
                 updateEncodedSurroundOutput();
                 sendEnabledSurroundFormats(mContentResolver, mSurroundModeChanged);
                 updateAssistantUIdLocked(/* forceUpdate= */ false);
-            }
-            boolean linkNotificationWithVolume = Settings.Secure.getInt(mContentResolver,
-                    Settings.Secure.VOLUME_LINK_NOTIFICATION, 1) == 1;
-            if (linkNotificationWithVolume != mLinkNotificationWithVolume) {
-                mLinkNotificationWithVolume = linkNotificationWithVolume;
-                onInitStreamsAndVolumes();
-                updateStreamVolumeAlias(true, TAG);
             }
         }
 
