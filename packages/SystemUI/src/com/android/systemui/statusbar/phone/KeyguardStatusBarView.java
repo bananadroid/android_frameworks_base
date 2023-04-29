2ea9f0a5033aca43833b385833881e2b04ec2a24
Pranav Vashi <neobuddy89@gmail.com>
2023-04-30 02:15:50 +0530

    KeyguardStatusBarView: Clean up cruft
    * We do not need to set carrier text margin as it was set properly in
      https://github.com/crdroidandroid/android_frameworks_base/commit/50ed26d88d0280caf2b166edeb4ae8b5cb217deb
    * Carrier text follows TextAppearance.StatusBar.Clock style, hence remove setting font size in code
    * Remove unused rounded corner padding leftover
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -100,7 +100,6 @@ public class KeyguardStatusBarView extends RelativeLayout {
     private int mCutoutSideNudge = 0;
 
     private DisplayCutout mDisplayCutout;
-    private int mRoundedCornerPadding = 0;
     // right and left padding applied to this view to account for cutouts and rounded corners
     private Pair<Integer, Integer> mPadding = new Pair(0, 0);
 
@@ -168,18 +167,6 @@ public class KeyguardStatusBarView extends RelativeLayout {
                 mStatusIconContainer.getPaddingBottom()
         );
 
-        // Respect font size setting.
-        mCarrierLabel.setTextSize(TypedValue.COMPLEX_UNIT_PX,
-                getResources().getDimensionPixelSize(
-                        com.android.internal.R.dimen.text_size_small_material));
-        lp = (MarginLayoutParams) mCarrierLabel.getLayoutParams();
-
-        int marginStart = calculateMargin(
-                getResources().getDimensionPixelSize(R.dimen.keyguard_carrier_text_margin),
-                mPadding.first);
-        lp.setMarginStart(marginStart);
-
-        mCarrierLabel.setLayoutParams(lp);
         updateKeyguardStatusBarHeight();
     }
 
@@ -203,8 +190,6 @@ public class KeyguardStatusBarView extends RelativeLayout {
                 R.dimen.ongoing_appops_dot_min_padding);
         mCutoutSideNudge = getResources().getDimensionPixelSize(
                 R.dimen.display_cutout_margin_consumption);
-        mRoundedCornerPadding = res.getDimensionPixelSize(
-                R.dimen.rounded_corner_content_padding);
     }
 
     private void updateVisibilities() {
