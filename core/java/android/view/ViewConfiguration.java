1b830c8bbae9ea41feab2a4bcb48092ebc1d9540
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-24 13:08:11 +0800

    ViewConfiguration: Update fling velocity for modern devices
    * MINIMUM_FLING_VELOCITY - based on fingeprint enrollment icon size in dips
    * MAXIMUM_FLING_VELOCITY - the tick distance can  be more than the default maximum velocity (on devices with higher resolutions the tick distance can reach 3x the maximum velocity defined on AOSP), increase it to a sane value to prevent choppy/janky fling animation
    
    Change-Id: Ice940cc42859833bdc75b08ce3aaff73eb064b69
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/view/ViewConfiguration.java
+++ core/java/android/view/ViewConfiguration.java
@@ -219,12 +219,12 @@ public class ViewConfiguration {
     /**
      * Minimum velocity to initiate a fling, as measured in dips per second
      */
-    private static final int MINIMUM_FLING_VELOCITY = 50;
+    private static final int MINIMUM_FLING_VELOCITY = 70;
 
     /**
      * Maximum velocity to initiate a fling, as measured in dips per second
      */
-    private static final int MAXIMUM_FLING_VELOCITY = 8000;
+    private static final int MAXIMUM_FLING_VELOCITY = 21000;
 
     /**
      * Delay before dispatching a recurring accessibility event in milliseconds.
