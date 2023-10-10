0ec8fa3e94cd32ae482ddd4b60e0b66c0a38c30b
Jing Ji <jji@google.com>
2023-10-10 16:31:35 +0000

    Make sure we'll always kill process group
    Bug: 302174571
    Test: Manual
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:00f95558bec4aed9bb7177ed47974dfd19834f6d)
    Merged-In: Id7c7b0dfec432809961042ceba47d7ca1393b968
    Change-Id: Id7c7b0dfec432809961042ceba47d7ca1393b968

--- services/core/java/com/android/server/am/ProcessRecord.java
+++ services/core/java/com/android/server/am/ProcessRecord.java
@@ -1252,11 +1252,10 @@ class ProcessRecord implements WindowProcessListener {
             killProcessGroup = true;
         }
         if (killProcessGroup) {
-            if (async) {
-                ProcessList.killProcessGroup(uid, mPid);
-            } else {
+            if (!async) {
                 Process.sendSignalToProcessGroup(uid, mPid, OsConstants.SIGKILL);
             }
+            ProcessList.killProcessGroup(uid, mPid);
         }
     }
 
