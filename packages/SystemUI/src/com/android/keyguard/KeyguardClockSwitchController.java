2812e9b55c5fb1d14b816765fb79bab16b4a3663
minaripenguin <minaripenguin@users.noreply.github.com>
2024-02-02 12:20:59 +0800

    Lockscreen Clock: Fix custom clock time updates
    committer: rmp
    
    approved by: hydrangea
    
    test: manual
    
    Change-Id: I5cbfb713fca9affcd661bb10fc58f9966740e3c3
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitchController.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitchController.java
@@ -67,6 +67,8 @@ import com.android.systemui.util.ViewController;
 import com.android.systemui.util.concurrency.DelayableExecutor;
 import com.android.systemui.util.settings.SecureSettings;
 
+import com.android.systemui.banana.ClockStyle;
+
 import java.io.PrintWriter;
 import java.util.Locale;
 import java.util.function.Consumer;
@@ -532,6 +534,9 @@ public class KeyguardClockSwitchController extends ViewController<KeyguardClockS
         if (clock != null) {
             clock.getSmallClock().getEvents().onTimeTick();
             clock.getLargeClock().getEvents().onTimeTick();
+        }
+        if (mCustomClockFrame != null) {
+        	((ClockStyle) mCustomClockFrame).onTimeChanged();
         }
     }
 
